d9c189f5c596cfccb6ccd93b19ce8150a973aaa0
Шишенко Сергей <sshishenko@sd.net>
2018-05-22 08:21:27 +0400

    Кнопка выход index

--- index.php
+++ index.php
@@ -4892,6 +4892,7 @@
         <div class="container">
             <p class="text-center">Сайт был создан проектным подразделением компании "София-Декор" <a href="http://www.sofia-decor.ru">(http://www.sofia-decor.ru)</a> под руководством Черкашина Д.В.</p>
             <p class="text-center">Разработчик Шишенко С.В. <a href="mailto:serge-meb@mail.ru?subject=Сайт 500.СТП"> (отправить вопрос или предложение)</a></p>
+            <p><button type="button" class="btn btn-default center" onclick="location.href='exit.php';">  <span class="glyphicon glyphicon-log-out" aria-hidden="true"></span> Выйти</button></p>
         </div>
     </footer> 
     <script src="./dist/js/jquery.min.js"></script>
