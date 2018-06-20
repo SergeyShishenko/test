9e7f3a60931da4bdde720e4024a8652feeea443d
Шишенко Сергей <sshishenko@sd.net>
2018-06-20 10:52:35 +0400

    mail.php

--- RECALL/mail.php
+++ RECALL/mail.php
@@ -3,23 +3,46 @@
 $message = "Имя: {$_POST['name']}<br>"; // добавляем имя в текст
 $message .= "Телефон: {$_POST['phone']}"; // добавляем телефон в текст
 
-require 'phpmailer/PHPMailerAutoload.php';
+ require 'phpmailer/PHPMailerAutoload.php';
+//require 'class.phpmailer.php';
+//require 'class.smtp.php';
 
 $mail = new PHPMailer;
 $mail->isSMTP();
 
-$mail->Host = 'smtp.mail.ru';
+$mail->Host = 'mail.500stp.website';//'mail.500stp.website';//
 $mail->SMTPAuth = true;
-$mail->Username = 'serge-meb'; // логин от вашей почты
-$mail->Password = '20meb14'; // пароль от почтового ящика
+$mail->Username = 'info@500stp.website'; // логин от вашей почты
+$mail->Password = '_MnExd3G'; // пароль от почтового ящика
 // $mail->Username = 'info'; // логин от вашей почты
 // $mail->Password = '_MnExd3G'; // пароль от почтового ящика info@500stp.website
-$mail->SMTPSecure = 'ssl';
+$mail->SMTPSecure = 'ssl'; //'tls'; //
 $mail->Port = '465';
 $mail->CharSet = 'UTF-8';
-$mail->From = 'serge-meb@mail.ru';
-$mail->FromName = 'Сергей';
-$mail->addAddress('serge-meb@mail.ru', 'Сергей');
+$mail->From = 'info@500stp.website';//Ваш Email
+//$mail->FromName = 'Сергей';
+$mail->addAddress('serge-meb@mail.ru');// Email получателя
+
+$mail->SMTPOptions = array(
+'ssl' => array(
+'verify_peer' => false,
+'verify_peer_name' => false,
+'allow_self_signed' => true
+)
+); 
+
+// // Прикрепление файлов
+// for ($ct = 0; $ct < count($_FILES[‘userfile’][‘tmp_name’]); $ct++) {
+// 	$uploadfile = tempnam(sys_get_temp_dir(), sha1($_FILES[‘userfile’][‘name’][$ct]));
+// 	$filename = $_FILES[‘userfile’][‘name’][$ct];
+// 	if (move_uploaded_file($_FILES[‘userfile’][‘tmp_name’][$ct], $uploadfile)) {
+// 	$mail->addAttachment($uploadfile, $filename);
+// 	} else {
+// 	$msg .= ‘Failed to move file to ‘ . $uploadfile;
+// 	}
+// 	} 
+
+// Письмо
 $mail->isHTML(true);
 $mail->Subject = 'Заказ обратного звонка';
 $mail->Body = $message;
@@ -28,4 +51,6 @@ if( $mail->send() ){
 	echo '<p style="color: green;">Ваше сообщение отправлено</p>';
 }else{
 	echo '<p style="color: red;">Ошибка!</p>';
+	echo '<p style="color: red;">'.$mail->ErrorInfo.'</p>';
+	
 }
