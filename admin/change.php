d2f680001c79c514414b23e4d661792fa5624b38
Шишенко Сергей <sshishenko@sd.net>
2018-11-01 15:06:56 +0400

    a

--- admin/change.php
+++ admin/change.php
@@ -34,8 +34,8 @@ if(isset($_POST["content_txt"]) && strlen($_POST["content_txt"])>0)
         $insert_id=$tbl;  
         if ($tbl=="obj" or $tbl=="obj_download" or $tbl=="obj_alias" or $tbl=="obj_in_addition" or $tbl=="obj_furnitur_prop")
         {
-            $parent[0]="obj_id";
-            $parent[1]=$id;
+            // $parent[0]="obj_id";
+            // $parent[1]=$id;
             // $insert_id="obj";
             if (isset($id))
             {
@@ -43,30 +43,33 @@ if(isset($_POST["content_txt"]) && strlen($_POST["content_txt"])>0)
                 // echo $insert_id ; //response
                 // exit();
                 // добавляем новую запись dj все таблицы объекта
-                $sql = "INSERT INTO  `obj`(`grupp_id`,`path_img_obj`,`fname_img_obj`,`fname_img_smoll_obj`,`template_obj`,`img_orientation_obj`) VALUES ('./dist/images/','test.png','test.png','shablony-dokumentov.php','Альбомная')";
+                $sql = "INSERT INTO  `obj`(`grupp_id`,`path_img_obj`,`fname_img_obj`,`fname_img_smoll_obj`,`template_obj`,`img_orientation_obj`) VALUES ('$parent[1]','./dist/images/','test.png','test.png','shablony-dokumentov.php','album')";
                 if(mysqli_query($dbconn,$sql))
                 {
                     //Record is successfully inserted, respond to ajax request
                     $insert_id = mysqli_insert_id($dbconn); //Get ID of last inserted record from MySQL 
-                    $parent[1] = $insert_id;
-                    $sql = "INSERT INTO  `obj_alias`(`obj_id`) VALUES ('$insert_id')";
-
-                    if ($result = mysqli_query($dbconn, "INSERT INTO  `obj_alias`(`obj_id`) VALUES ('$insert_id')")) {
-                        /* очищаем результирующий набор */
-                        mysqli_free_result($result);
-                    }else{header('HTTP/1.1 500 Looks like mysql error, could not insert record!'.mysqli_error($dbconn));exit();}
-                    if ($result = mysqli_query($dbconn, "INSERT INTO  `obj_download`(`obj_id`) VALUES ('$insert_id')")) {
-                        /* очищаем результирующий набор */
-                        mysqli_free_result($result);
-                    }else{header('HTTP/1.1 500 Looks like mysql error, could not insert record!'.mysqli_error($dbconn));exit();}
-                    if ($result = mysqli_query($dbconn, "INSERT INTO  `obj_furnitur_prop`(`obj_id`) VALUES ('$insert_id')")) {
-                        /* очищаем результирующий набор */
-                        mysqli_free_result($result);
-                    }else{header('HTTP/1.1 500 Looks like mysql error, could not insert record!'.mysqli_error($dbconn));exit();}
-                    if ($result = mysqli_query($dbconn, "INSERT INTO  `obj_in_addition`(`obj_id`) VALUES ('$insert_id')")) {
-                        /* очищаем результирующий набор */
-                        mysqli_free_result($result);
-                    }else{header('HTTP/1.1 500 Looks like mysql error, could not insert record!'.mysqli_error($dbconn));exit();}
+                    // $parent[1] = $insert_id;
+                    // $sql = "INSERT INTO  `obj_alias`(`obj_id`) VALUES ('$insert_id')";
+                    mysqli_query($dbconn, "INSERT INTO  `obj_alias`(`obj_id`) VALUES ('$insert_id')");
+                    // if ($result = mysqli_query($dbconn, "INSERT INTO  `obj_alias`(`obj_id`) VALUES ('$insert_id')")) {
+                    //     /* очищаем результирующий набор */
+                    //     mysqli_free_result($result);
+                    // }else{header('HTTP/1.1 500 Looks like mysql error, could not insert record!'.mysqli_error($dbconn));exit();}
+                    mysqli_query($dbconn, "INSERT INTO  `obj_download`(`obj_id`) VALUES ('$insert_id')");
+                    // if ($result = mysqli_query($dbconn, "INSERT INTO  `obj_download`(`obj_id`) VALUES ('$insert_id')")) {
+                    //     /* очищаем результирующий набор */
+                    //     mysqli_free_result($result);
+                    // }else{header('HTTP/1.1 500 Looks like mysql error, could not insert record!'.mysqli_error($dbconn));exit();}
+                    mysqli_query($dbconn, "INSERT INTO  `obj_furnitur_prop`(`obj_id`) VALUES ('$insert_id')");
+                    // if ($result = mysqli_query($dbconn, "INSERT INTO  `obj_furnitur_prop`(`obj_id`) VALUES ('$insert_id')")) {
+                    //     /* очищаем результирующий набор */
+                    //     mysqli_free_result($result);
+                    // }else{header('HTTP/1.1 500 Looks like mysql error, could not insert record!'.mysqli_error($dbconn));exit();}
+                    mysqli_query($dbconn, "INSERT INTO  `obj_in_addition`(`obj_id`) VALUES ('$insert_id')");
+                    // if ($result = mysqli_query($dbconn, "INSERT INTO  `obj_in_addition`(`obj_id`) VALUES ('$insert_id')")) {
+                    //     /* очищаем результирующий набор */
+                    //     mysqli_free_result($result);
+                    // }else{header('HTTP/1.1 500 Looks like mysql error, could not insert record!'.mysqli_error($dbconn));exit();}
 
                     // mysqli_close($dbconn);                 
                     
@@ -85,9 +88,9 @@ if(isset($_POST["content_txt"]) && strlen($_POST["content_txt"])>0)
         $sql = "INSERT INTO  `$tbl`(`$field`,`$parent[0]`) VALUES ('$contentToSave','$parent[1]')";
         // $insert_id=$insert_id.'_'.'00';
         
-        header('HTTP/1.1 400 Zapros dla dobavleniy! '.$sql);
-        // echo $insert_id ; //response
-        exit();
+        // header('HTTP/1.1 400 Zapros dla dobavleniy! '.$sql);
+        echo $insert_id ; //response
+        // exit();
 
         
         // "INSERT INTO head(name_head) VALUES('".$contentToSave."')"
@@ -106,7 +109,7 @@ if(isset($_POST["content_txt"]) && strlen($_POST["content_txt"])>0)
         //Record is successfully inserted, respond to ajax request
         $insert_id = mysqli_insert_id($dbconn); //Get ID of last inserted record from MySQL 
         mysqli_close($dbconn);
-        echo $insert_id ; //response
+        // echo $insert_id ; //response
 
     }else{
         //вывод ошибки
