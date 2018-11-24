21546e5f0b8e035dbfef44a81ae78baac8a1a75c
SergeyShishenko <serge-meb@mail.ru>
2018-11-24 15:48:51 +0300

    openserver

--- inc-session-start.php
+++ inc-session-start.php
@@ -5,6 +5,7 @@ ini_set('session.cookie_lifetime', 43200);
 session_start();
 $_SESSION['ref'] = $_SERVER['REQUEST_URI'];
 define('__ROOT__', dirname(dirname(__FILE__))); 
+// echo __ROOT__.'/DATA/data.php';
 require_once(__ROOT__.'/DATA/data.php'); 
 // require_once('DATA/data.php');
 if (!isset($_SESSION['sess_login']) || !isset($_SESSION['sess_pass']) || $_SESSION['sess_login']!==$enter_login || $_SESSION['sess_pass']!==$enter_passw) { header('Location:index-session.php');    exit();}
