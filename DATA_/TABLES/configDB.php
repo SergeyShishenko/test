737cbbbbbd466c3c8197955e425e4dc9b9706e21
SergeyShishenko <37832711+SergeyShishenko@users.noreply.github.com>
2018-11-25 19:42:54 +0300

    Update configDB.php

--- DATA_/TABLES/configDB.php
+++ DATA_/TABLES/configDB.php
@@ -2,10 +2,7 @@
 
 function dbconnect()
     {
-    $dbHost='localhost';
-    $mysql_user ='u0474172_default';
-    $mysql_password ='_MnExd3G';
-    $my_db ='u0474172_default';
+
     $dbconn=mysqli_connect($dbHost, $mysql_user, $mysql_password, $my_db )or die('не удалось подключиться к базе данных');
     
     return $dbconn; 
