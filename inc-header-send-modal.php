2839dec916cd61ee7185b5cbedcdb89224408c76
Шишенко Сергей <sshishenko@sd.net>
2018-11-27 11:45:02 +0400

    send

--- inc-header-send-modal.php
+++ inc-header-send-modal.php
@@ -6,7 +6,7 @@
     <div class="modal-dialog"  role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close" style="margin-top: 0px;">
                     <span aria-hidden="true">&times;</span>
                 </button>
                 <h4 class="modal-title" id="myModalLabel"> Отправка сообщения автору сайта</h4>
