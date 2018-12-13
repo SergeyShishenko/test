b942e41e980df36b84b293475a2d2e8af9ae7b2e
Шишенко Сергей <sshishenko@sd.net>
2018-12-13 10:36:12 +0400

    kd

--- shablony-dokumentov-titulnyy-kd.php
+++ shablony-dokumentov-titulnyy-kd.php
@@ -35,7 +35,7 @@ include 'inc-left-side-menu-shablony-dokumentov.php';
                                 <div class="overlay"></div>                  
                             <p> <a>
                                 <!-- <img src="./dist/images/shablon-kd.png" class="center-block img-rounded img-thumbnail" -->
-                                <img  src=data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==  data-src="./dist/images/shablon-kd_1-v4.png" class="center-block img-rounded img-thumbnail b-lazy img-album"
+                                <img  src=data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==  data-src="./dist/images/shablon-kd_1-v5.png" class="center-block img-rounded img-thumbnail b-lazy img-album"
                                  data-toggle="modal"
                                  data-target="#myModal"
                                                             
