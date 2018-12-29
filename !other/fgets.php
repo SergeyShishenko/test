b797af9f34e902ccc3b12a3df4dff1ed1c3e6c41
Шишенко Сергей <sshishenko@sd.net>
2018-12-29 15:07:13 +0400

    fgets

--- !other/fgets.php
+++ !other/fgets.php
@@ -3,17 +3,29 @@ $f = file_get_contents('_2312.csv');
 $f = iconv("WINDOWS-1251", "UTF-8", $f);
 file_put_contents('zakaz_2312.csv', $f);
 $handle = @fopen("zakaz_2312.csv", "r");
+echo '<table border="1" ><tr>'; 
 if ($handle) {
     while (($buffer = fgets($handle, 4096)) !== false) {
         // echo $buffer . "<hr>";
         list($client,$address,$number_order,,,$complect,$product,,,,,$def,,,,,,,,,,$floor,$room,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,$end) = explode(';', $buffer);
-        echo "<hr>";
-        echo $client." | ".$address." | ".$number_order." | ".$complect." | ".$product." | ".$def." | ".$floor." | ".$room; // hello, world
+        $data=array($client,$address,$number_order,$complect,$product,$def,$floor,$room);
+
+        $num = count ($data); //полей в строке $row
+        for ($c=0; $c < $num; $c++) { 
+        // output data    
+        // echo "<td style='white-space: nowrap;'>". $data[$c]." </td>"; 
+        echo "<td>". $data[$c]." </td>"; 
+        } 
+        echo "</tr><tr>"; 
+
+      
+        // echo $client." | ".$address." | ".$number_order." | ".$complect." | ".$product." | ".$def." | ".$floor." | ".$room; // hello, world
         // echo "<hr>";
     }
     if (!feof($handle)) {
         echo "Ошибка: fgets() неожиданно потерпел неудачу\n";
     }
+    echo '</tr></table>'; 
     fclose($handle);
 }
 ?>
