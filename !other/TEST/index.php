173db835b342a11e9b90fc2cd6d4ca01d681f351
Шишенко Сергей <sshishenko@sd.net>
2019-01-14 12:57:35 +0400

    test

---
+++ !other/TEST/index.php
@@ -0,0 +1,21 @@
+﻿<?php
+// $great = 'здорово';
+// echo "Это { $great}";
+
+// $hello = 'Hello!';
+// for($i = 0; $i <= 3; $i++)
+// {
+// 	echo $hello[$i];
+// }
+// echo $hello[strlen($hello)-1];
+
+// $arr = [0 => "H", 1 => "e", 2 => "l", 3 => "l", 4 => "o", 5 => "!"];
+// krsort($arr);
+// foreach ($arr as $key => $value)
+// {
+//    if($value)
+//       echo $value;
+// }
+
+echo 'Однажды Арнольд сказал: "I\'ll be back"'
+?>
