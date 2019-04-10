01286098c2f200173675f15d1ba7c9896e4a501a
SergeyShishenko <serge-meb@mail.ru>
2019-04-10 21:58:04 +0300

    mynumbers

--- !other/alternation.js
+++ !other/alternation.js
@@ -1,8 +1,22 @@
-var mynumbers=new Array(1,2,3,6,8,9,10,12);
-console.log("Начало первой группы: "+mynumbers[0]);
-for(var i = 1; i < mynumbers.length; i++) {
+// var mynumbers=new Array(1,2,3,6,8,9,10,12);
+// var mynumbers=new Array(1,2,3,6,8,9,10,12,14);
+var mynumbers=new Array(1,2,3,4,5,6,7,8,9,10,11,12);
+var endgroupp = mynumbers[1];
+var first = mynumbers[0];
+// console.log("Начало первой группы: "+mynumbers[0]);
+var str=mynumbers[0];
+for(var i = 1; i < mynumbers.length; i++) {    
     if(mynumbers[i] - mynumbers[i-1] != 1) {
-        console.log("Начало следующей группы: "+mynumbers[i]);
-        // break;
+         if(first !=endgroupp) {str=str+"-"+endgroupp;}       
+        // console.log("Конец группы: "+endgroupp);
+        // console.log("Начало следующей группы: "+mynumbers[i]);
+        first=mynumbers[i];
+        str=str+","+mynumbers[i];
+        
     }
+       endgroupp=mynumbers[i];
 }
+
+if(first !=endgroupp){str=str+"-"+endgroupp;}
+
+console.log(str);
