0d1675a947e0dd7d430fee1a51d4cf271b14941c
Шишенко Сергей <sshishenko@sd.net>
2019-05-27 14:50:43 +0400

    dvuhstoronnyaya-d10

--- furnitura-dlya-mebeli-styazhka-konicheskaya-dvuhstoronnyaya-d10-t25.php
+++ furnitura-dlya-mebeli-styazhka-konicheskaya-dvuhstoronnyaya-d10-t25.php
@@ -99,13 +99,13 @@ include 'inc/inc-left-side-menu-furnitura.php';
                                                             <td>Металл</td>
                                                             <td  colspan="3">Шток двухсторонний стяжки конической D10, 62&nbsp;мм</td>
                                                         </tr>
-                                                        <tr   >
+                                                        <!-- <tr   >
                                                                 <td><img src="./dist/filesdb/images/pagevpi/ArticulFutorka-BU12.png" class="" alt="Футорка" title=""></td>
                                                                 <td>2</td>
                                                                 <td class="" >BU12 (82-02)</td>
                                                                 <td>Желтый</td> 
                                                                 <td colspan="3">Футорка металлическая с распорным шариком M6</td>
-                                                        </tr>
+                                                        </tr> -->
                                                       
                                                         
                                                         </tbody>
@@ -414,108 +414,7 @@ include 'inc/inc-left-side-menu-furnitura.php';
                                 </div>
                             <!-- /часть --> 
 
-                            <!-- часть -->
-                                 <h2 >Футорка металлическая с распорным шариком <br>для штоков стяжки эксцентриковой и конической с резьбой М6</h2>                         
-                                 
-                                <div class="row" style="margin-bottom: 15px; margin-left: 15px;">   
-                                    <div id="carousel4" class="carousel slide" data-ride="carousel" style="display: inline-block;">
-                                        <div class="carousel-inner">
-
-                                            <div class="item active">
-                                                <a class="thumbb" href="#" data-image-id="" data-toggle="modal" data-title="Футорка металлическая с распорным шариком M6" data-image="./dist/filesdb/images/Futorka-BU12.png" data-target="#image-gallery">
-                                                    <img  
-                                                        src="./dist/filesdb/images/Futorka-BU12.png"
-                                                        data-src="./dist/filesdb/images/Futorka-BU12.png"
-                                                        class="center-block   img-rounded    bar "
-                                                        data-toggle="modal"                                                                                                                    
-                                                        alt="Футорка металлическая с распорным шариком M6"
-                                                        data-pdf=""                                                    
-                                                    >
-                                                </a>
-                                            </div>
-                                            <div class="item">
-                                                <a class="thumbb" href="#" data-image-id="" data-toggle="modal" data-title="Необходимое пространство" data-image="./dist/filesdb/images/Futorka1-960x640.png" data-target="#image-gallery">
-                                                    <img  
-                                                        src="./dist/filesdb/images/Futorka1-960x640.png"
-                                                        data-src="./dist/filesdb/images/Futorka1-960x640.png"
-                                                        class="center-block   img-rounded     bar "
-                                                        data-toggle="modal"                                                                                                                          
-                                                        alt="Необходимое пространство"                                                        
-                                                    >
-                                                </a>
-                                            </div>
-                                            <div class="item">
-                                                <a class="thumbb" href="#" data-image-id="" data-toggle="modal" data-title="Информация для заказа" data-image="./dist/filesdb/images/Futorka2-960x640.png" data-target="#image-gallery">
-                                                    <img  
-                                                        src="./dist/filesdb/images/Futorka2-960x640.png"
-                                                        data-src="./dist/filesdb/images/Futorka2-960x640.png"
-                                                        class="center-block   img-rounded     bar "
-                                                        data-toggle="modal"                                                                                                                          
-                                                        alt="Информация для заказа"                                                        
-                                                    >
-                                                </a>
-                                            </div>
-                                        
-                                            
-                                        </div>
-                                        <!-- Элементы управления -->
-                                        <a class="left carousel-control" href="#carousel4" role="button" data-slide="prev">
-                                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                                            <span class="sr-only">Предыдущий</span>
-                                        </a>
-                                        <a class="right carousel-control" href="#carousel4" role="button" data-slide="next">
-                                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                                            <span class="sr-only">Следующий</span>
-                                        </a>
-                                    </div>
-                                </div> 
-
-                                <div class="row" style="margin-bottom: 15px; margin-left: 15px;">
-                                    <div class="row col-xs-12 col-sm-12 col-md-12" style="background: #fff;
-                                                border: 1px solid rgb(221, 221, 221);
-                                                box-shadow: 0.5em 0.5em 10px rgba(100,100,100,0.3);
-                                                border-radius: 4px;
-                                                padding: 15px;
-                                                ">
-
-                                        <div class="row col-xs-12 col-sm-12 col-md-12" style="margin-top: 20px;">                                                           
-                                            <div class="row">                                                
-                                                <table class="table table-striped table-responsive">
-                                                    
-                                                    <tbody> 
-
-                                                        <tr>
-                                                        <th>M,<br>мм</th>
-                                                        <th>Цвет</th>
-                                                        <th>L,<br>мм</th>
-                                                        <th>D,<br>мм</th>
-                                                        <th>S,<br>мм</th>
-                                                        <th>E,<br>мм</th>
-                                                        <th>Артикул</th>
-                                                        </tr>
-
-                                                       
-                                                        <tr>
-                                                        <td>6</td>
-                                                        <td>желтый</td>
-                                                        <td>12</td>
-                                                        <td>8</td>
-                                                        <td>8</td>
-                                                        <td>9</td>
-                                                        <td><span class="boldstyle">BU12 (82-02)</span></td>
-                                                        </tr>
-
-
-                                                    </tbody>
-
-                                                </table>
-                                            </div>                              
-                                        </div>
-
-                                    </div>
-
-                                </div>
-                            <!-- /часть --> 
+                           
 
 
 
