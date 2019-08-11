03f3f60db0488a2c57e58275eaedc0cbf3ea269f
SergeyShishenko <serge-meb@mail.ru>
2019-08-11 12:42:12 +0300

    og

--- inc/inc-head.php
+++ inc/inc-head.php
@@ -1,28 +1,41 @@
 <?php 
 include 'inc/inc-version-css-js.php'; 
 echo'<!DOCTYPE html>
-<html lang="ru-RU">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>500.СТП</title>
-        <link href="./dist/css/bootstrap.css" rel="stylesheet">
-        <link href="./dist/css/animate.min.css" rel="stylesheet">       
-        <link href="./dist/css/docs.css?v='. $docsVersion .'" rel="stylesheet">
-        <link href="./dist/css/docs-first.css?v='. $docsfirstVersion .'" rel="stylesheet">
-        <!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">-->
-        <link rel="stylesheet" href="./dist/css/font-awesome.min.css">
-        <!-- генерация -->
-        <link href="./dist/css/uploadify.css" rel="stylesheet">
-        <link href="./dist/css/style.css" rel="stylesheet">
-        <link href="./dist/css/drop.css" rel="stylesheet">
-        <link href="./dist/css/submit-process.css" rel="stylesheet">
-        <link href="./dist/css/font-awesome.css" rel="stylesheet">
+<html lang="ru-RU"  prefix="og: http://ogp.me/ns#">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <meta name="viewport" content="width=device-width"/>
+    <meta property="og:title" content="СТАНДАРТЫ ПРЕДПРИЯТИЯ" />
+    <meta property="og:image" content="https://www.500stp.website/dist/css/prew500stp.jpg" />
+    <meta property="og:image:width" content="300" />
+    <meta property="og:image:height" content="300" />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="СТАНДАРТЫ ПРЕДПРИЯТИЯ">
+    <meta property="og:description" content="Стандарт предприятия (СТП) необходим для организации четкой и качественной работы, для поддержания успешного и эффективного функционирования компании" />
+    <meta property="og:url" content= "https://500stp.website" />
+    <meta property="og:image:type" content="image/jpeg" />
+    <meta property="og:locale" content="ru_RU" />
+    <link rel="image_src" href="https://www.500stp.website/dist/css/prew500stp.jpg"/>
+    <meta name="yandex-verification" content="07a17e6c1df2c2b2" />
 
-        <link rel="shortcut icon" href="shesterenka.png" type="image/x-icon">  
-        <script>
-            if (!name) {name = new Date ().getTime (); location.reload (true)}
-        </script>   
+    <title>500.СТП</title>
+    <link href="./dist/css/bootstrap.css" rel="stylesheet">
+    <link href="./dist/css/animate.min.css" rel="stylesheet">       
+    <link href="./dist/css/docs.css?v='. $docsVersion .'" rel="stylesheet">
+    <link href="./dist/css/docs-first.css?v='. $docsfirstVersion .'" rel="stylesheet">
+    <!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">-->
+    <link rel="stylesheet" href="./dist/css/font-awesome.min.css">
+    <!-- генерация -->
+    <link href="./dist/css/uploadify.css" rel="stylesheet">
+    <link href="./dist/css/style.css" rel="stylesheet">
+    <link href="./dist/css/drop.css" rel="stylesheet">
+    <link href="./dist/css/submit-process.css" rel="stylesheet">
+    <link href="./dist/css/font-awesome.css" rel="stylesheet">
+
+    <link rel="shortcut icon" href="shesterenka.png" type="image/x-icon">  
+    <script>
+        if (!name) {name = new Date ().getTime (); location.reload (true)}
+    </script>   
     
 ';?>
