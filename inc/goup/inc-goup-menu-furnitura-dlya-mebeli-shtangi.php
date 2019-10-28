fced65f2984da18c2f26192dc98e6019c188091c
Шишенко Сергей <sshishenko@sd.net>
2019-10-28 10:05:24 +0400

    ovalnaya-shtanga

--- inc/goup/inc-goup-menu-furnitura-dlya-mebeli-shtangi.php
+++ inc/goup/inc-goup-menu-furnitura-dlya-mebeli-shtangi.php
@@ -226,6 +226,128 @@ $path_img_carousel="./dist/filesdb/images/carousel/shtangi/";//800x800 и 960x64
                   
                     
     ];
+    $shtangi[3]=['path'=>"furnitura-dlya-mebeli-shtangi5.php",
+    'id'=> "#ovalnaya-shtanga",
+    'img'=> $path_img."tbsHW_012_1930_PR-1.png", //200x200 для поиска
+    'keywords'=>"",
+    'articul_500STP'=>"",
+    'def'=>"Штанга овальная 15х30, хром, L-3000",
+    'composition_article'=>array( // таблица состава артикула
+                                    array(                            
+                                            'img'=>$path_img_composition_article."ArticulHW_012_1930_PR-1.png",//100х67 для VPI
+                                            'alt'=>"Профиль-штанга",
+                                            'count'=> 1,// количество
+                                            'supplier_articul'=>"HW.012.1930.PR",//артикул поставщика
+                                            'id_table'=>"00",//data-objfurnid
+                                            'href_articul'=>"https://makmart.ru/WItem/12429/" ,//страница фурнитуры                            
+                                            'color_material'=>"алюминий",//цвет/материал
+                                            'def'=>"Профиль-штанга 1930 для LED подсветки, L=2000 мм"  //описание                         
+                                            ),
+                                        array(                            
+                                            'img'=>$path_img_composition_article."ArticulHW_012_1930_H1-1.png", //100х67 для VPI
+                                            'alt'=>"Штангодержатель",
+                                            'count'=> 2,// количество
+                                            'supplier_articul'=>"HW.012.1930.H1",//артикул поставщика
+                                            'id_table'=>"00",//data-objfurnid
+                                            'href_articul'=>"https://makmart.ru/WItem/12430/" ,//страница фурнитуры                            
+                                            'color_material'=>"пластик / серый",//цвет/материал
+                                            'def'=>"Штангодержатель для профиль-штанги 1930, цвет серый"  //описание                         
+                                        ),
+                                        array(                            
+                                            'img'=>$path_img_composition_article."ArticulHW_012_1930_C1-1.png", //100х67 для VPI
+                                            'alt'=>"Заглушка с отверстием",
+                                            'count'=> 1,// количество
+                                            'supplier_articul'=>"HW.012.1930.C1",//артикул поставщика
+                                            'id_table'=>"00",//data-objfurnid
+                                            'href_articul'=>"https://makmart.ru/WItem/12428/" ,//страница фурнитуры                            
+                                            'color_material'=>"пластик / серый",//цвет/материал
+                                            'def'=>"Заглушка с отверстием для профиль-штанги 1930, цвет серый"  //описание                         
+                                        ),
+                                        array(                            
+                                            'img'=>$path_img_composition_article."ArticulHW_012_1707_TR.png", //100х67 для VPI
+                                            'alt'=>"Профиль-рассеиватель",
+                                            'count'=> 1,// количество
+                                            'supplier_articul'=>"HW.012.1707.TR",//артикул поставщика
+                                            'id_table'=>"00",//data-objfurnid
+                                            'href_articul'=>"https://makmart.ru/WItem/8625/" ,//страница фурнитуры                            
+                                            'color_material'=>"Транспарент",//цвет/материал
+                                            'def'=>"Профиль-рассеиватель 1707 для LED подсветки, L=2000мм, отделка транспарент"  //описание                         
+                                        ),
+                                        array(                            
+                                            'img'=>$path_img_composition_article."ArticulHW_012_1707_FR.png", //100х67 для VPI
+                                            'alt'=>"Профиль-рассеиватель",
+                                            'count'=> 1,// количество
+                                            'supplier_articul'=>"HW.012.1707.FR",//артикул поставщика
+                                            'id_table'=>"00",//data-objfurnid
+                                            'href_articul'=>"https://makmart.ru/WItem/8626/" ,//страница фурнитуры                            
+                                            'color_material'=>"Матовый",//цвет/материал
+                                            'def'=>"Профиль-рассеиватель 1707 для LED подсветки, L=2000мм, отделка матовый"  //описание                         
+                                            )
+                            
+                                ),// состав артикула  
+    //характеристики 
+    'harakteristiki_def'=>" <p>
+                                <ul class='stat' style='margin-top: 0px;'>
+                                    <li><b>Светильник штанга для гардероба</b></li>
+                                    <li>Материал: Алюминий.</li> 
+                                    <li>Цвет: Алюминий.</li>
+                                    <li>Длина: 2000 мм.</li>
+                                    <li>Размер в сечении: 30 х 19 мм.                                                   
+                                    <li>Штанга имеет конструкцию для размещения в ней светодиодной  LED ленты  шириной 8/10 мм.</li>                                                                                                                                                                                
+                                                                                                                                                                 
+                                </ul>
+                            </p>", 
+    //инструкции                         
+    'instrukciya_def'=>"<h4 >Проектирование</h3>
+                        <h5 >Лента светодиодная, источники питания и коннекторы, пожалуйста, отдельно.</h5>
+                        ", 
+    'instrukciya_carousel'=>array( // инструкция
+                                    array(                            
+                                            'img'=>$path_img_carousel."TA02STCR-1.png",//data-image, src, data-src  800x800 и 960x640                                                            
+                                            'title'=>"Профиль-штанга 1930 для LED подсветки, L=2000 мм, отделка алюминий"// data-title, alt
+                                    ),
+                                    array(                            
+                                            'img'=>$path_img_carousel."TA02STCR-2.png",//data-image, src, data-src  800x800 и 960x640                                                            
+                                            'title'=>"Профиль-штанга 1930 для LED подсветки, L=2000 мм, отделка алюминий"// data-title, alt
+                                    ),
+                                    array(                            
+                                            'img'=>$path_img_carousel."RT03ZNL-1.png",//data-image, src, data-src  800x800 и 960x640                                                            
+                                            'title'=>"Профиль-штанга 1930 для LED подсветки, L=2000 мм, отделка алюминий"// data-title, alt
+                                    ),
+                                    array(                            
+                                            'img'=>$path_img_carousel."RT03ZNL-2.png",//data-image, src, data-src  800x800 и 960x640                                                            
+                                            'title'=>"Профиль-штанга 1930 для LED подсветки, L=2000 мм, отделка алюминий"// data-title, alt
+                                    ),
+                                    array(                            
+                                            'img'=>$path_img_carousel."WA0612SCR-1.png",//data-image, src, data-src  800x800 и 960x640                                                            
+                                            'title'=>"Штангодержатель для профиль-штанги 1930, цвет серый"// data-title, alt
+                                    ),
+                                    array(                            
+                                            'img'=>$path_img_carousel."WA0612SCR-2.png",//data-image, src, data-src  800x800 и 960x640                                                            
+                                            'title'=>"Штангодержатель для профиль-штанги 1930, цвет серый"// data-title, alt
+                                    ),
+                                    array(                            
+                                            'img'=>$path_img_carousel."WA0612SCR-3.png",//data-image, src, data-src  800x800 и 960x640                                                            
+                                            'title'=>"Заглушка с отверстием для профиль-штанги 1930, цвет серый"// data-title, alt
+                                    ),
+                                    array(                            
+                                            'img'=>$path_img_carousel."WA0612CCR.png",//data-image, src, data-src  800x800 и 960x640                                                            
+                                            'title'=>"Заглушка с отверстием для профиль-штанги 1930, цвет серый"// data-title, alt
+                                    ),
+                                    array(                            
+                                            'img'=>$path_img_carousel."7030CR-1.png",//data-image, src, data-src  800x800 и 960x640                                                            
+                                            'title'=>"Профиль-рассеиватель 1707 для LED подсветки, L=2000мм, отделка транспарент"// data-title, alt
+                                    ),
+                                    array(                            
+                                            'img'=>$path_img_carousel."7030CR-2.png",//data-image, src, data-src  800x800 и 960x640                                                            
+                                            'title'=>"Профиль-рассеиватель 1707 для LED подсветки, L=2000мм, отделка матовый"// data-title, alt
+                                            )
+                                       
+                            
+                                )// инструкция               
+  
+    
+];
 
    
                 
