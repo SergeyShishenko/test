d18df228363121b70f70a5fe4300d0a85c1acd37
Шишенко Сергей <sshishenko@sd.net>
2019-11-22 11:26:50 +0400

    flip-vertical

--- !other/CNC/imagecreate.php
+++ !other/CNC/imagecreate.php
@@ -9,6 +9,17 @@ $red   = imagecolorallocate($img, 255,   0,   0);
 $green = imagecolorallocate($img,   0, 255,   0);
 $blue  = imagecolorallocate($img,   0,   0, 255);
 
+// imagearc ( resource $image , int $cx , int $cy , int $width , int $height , int $start , int $end , int $color ) : bool
+// imagearc() рисует дугу окружности с заданными координатами центра.
+// $image   Ресурс изображения, возвращаемый одной из функций создания изображений, например, такой как imagecreatetruecolor().
+// $cx      x-координата центра.
+// $cy      y-координата центра.
+// $width   Ширина дуги.
+// $height  Высота дуги.
+// $start   Угол начала дуги в градусах.
+// $end     Угол окончания дуги в градусах. 0° соответствует положению 3 часа, дуга рисуется по часовой стрелке.
+// $color   Идентификатор цвета, созданный функцией imagecolorallocate().
+
 // рисуем голову
 imagearc($img, 100, 100, 200, 200,  0, 360, $white);
 // рот
--- !other/CNC/index.php
+++ !other/CNC/index.php
@@ -5,8 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+    /* Для отражения по горизонтали */
+    .flip-horizontal{
+        -moz-transform: scale(-1, 1);
+        -webkit-transform: scale(-1, 1);
+        -o-transform: scale(-1, 1);
+        transform: scale(-1, 1);
+        filter: FlipH;
+        -ms-filter: "FlipH";
+        }
+    /* Для отражения по вертикали */
+    .flip-vertical{
+        -moz-transform: scale(1,-1);
+        -webkit-transform: scale(1, -1);
+        -o-transform: scale(1, -1);
+        transform: scale(1, -1);
+        filter: FlipV;
+        -ms-filter: "FlipV";
+        }
+    </style>
 </head>
 <body>
-<img id="capcha" src="imagecreate.php" height="200" width="200"/>
+<img id="capcha" src="imagecreate.php" height="200" width="200" class="flip-vertical"/>
 </body>
 </html>
