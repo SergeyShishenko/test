5c1f17f2d549211b4e69a543c66aa91d13d4169e
Шишенко Сергей <sshishenko@sd.net>
2019-11-22 14:55:59 +0400

    cnc

---
+++ !other/CNC/cnc.php
@@ -0,0 +1,15 @@
+<?php 
+$file = file_get_contents('6.1_13_1.CNC', FALSE, NULL, 34);//читаем с 35-го символа
+echo $file;
+
+
+echo '<br><br>'.'======file====='.'<br><br>';
+
+$names=file('6.1_13_1.CNC');
+// To check the number of lines
+echo "Строк в файле - ".count($names).'<br>';
+foreach($names as $name)
+{
+   echo $name.'<br>';
+}
+?>
