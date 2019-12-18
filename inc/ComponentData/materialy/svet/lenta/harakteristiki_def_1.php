c7f5b071e9079f222b9a69a4528ba1bb6443abba
Шишенко Сергей <sshishenko@sd.net>
2019-12-18 09:54:57 +0400

    harakteristiki_def

--- inc/ComponentData/materialy/svet/lenta/harakteristiki_def_1.php
+++ inc/ComponentData/materialy/svet/lenta/harakteristiki_def_1.php
@@ -1,142 +1,137 @@
 <?php
 $harakteristiki_def_1=" <div id='parameters' style='display: block;'>
-                        <table class='table table-striped table-responsive'>
-                                        <tbody><tr>
-                        <td>Артикул																</td>
-                                                        <td>013239(B ПРОВЕРКА)</td>
+                                <table class='table table-striped table-responsive'>
+                                        <tbody>
+                                                <tr>
+                                                        <td>Артикул</td>
+                                                        <td>013239()</td>
                                                 </tr>
-                                        <tr>
-                        <td>Цвет																</td>
+                                                <tr>
+                                                        <td>Цвет</td>
                                                         <td><div class='block-color-wrapper'><div class='block-color block-color-cool_white'>cool_white</div></div></td>
                                                 </tr>
-                                        <tr>
-                        <td>Тип товара																</td>
+                                                <tr>
+                                                        <td>Тип товара</td>
                                                         <td>Лента</td>
                                                 </tr>
-                                        <tr>
-                        <td>Класс пылевлагозащиты																</td>
+                                                <tr>
+                                                        <td>Класс пылевлагозащиты</td>
                                                         <td>IP20</td>
                                                 </tr>
-                                        <tr>
-                        <td>Длина волны																</td>
+                                                <tr>
+                                                        <td>Длина волны</td>
                                                         <td>Белый (холодный)</td>
                                                 </tr>
-                                        <tr>
-                        <td>Цветовая температура																</td>
+                                                <tr>
+                                                        <td>Цветовая температура</td>
                                                         <td>min: 8 100 K; typ: 8 000 K; max: 9 600 K</td>
                                                 </tr>
-                                        <tr>
-                        <td>Индекс цветопередачи																</td>
+                                                <tr>
+                                                        <td>Индекс цветопередачи</td>
                                                         <td>&gt;85</td>
                                                 </tr>
-                                        <tr>
-                        <td>Угол обзора																</td>
+                                                <tr>
+                                                        <td>Угол обзора</td>
                                                         <td>120 °</td>
                                                 </tr>
-                                        <tr>
-                        <td>Типоразмер светодиода																</td>
+                                                <tr>
+                                                        <td>Типоразмер светодиода</td>
                                                         <td>3528</td>
                                                 </tr>
-                                        <tr>
-                        <td>Плотность светодиодов																</td>
+                                                <tr>
+                                                        <td>Плотность светодиодов</td>
                                                         <td>120 шт/м</td>
                                                 </tr>
-                                        <tr>
-                        <td>Кол-во светодиодов																</td>
+                                                <tr>
+                                                        <td>Кол-во светодиодов</td>
                                                         <td>600 шт</td>
                                                 </tr>
-                                        <tr>
-                        <td>Световой поток на 1м																</td>
+                                                <tr>
+                                                        <td>Световой поток на 1м</td>
                                                         <td>max: 820 lm/m</td>
                                                 </tr>
-                                        <tr>
-                        <td>Световой поток																</td>
+                                                <tr>
+                                                        <td>Световой поток</td>
                                                         <td>max: 4 100 lm</td>
                                                 </tr>
-                                        <tr>
-                        <td>Мощность на 1м																</td>
+                                                <tr>
+                                                        <td>Мощность на 1м</td>
                                                         <td>typ: 8,4 W/m; max: 9,6 W/m</td>
                                                 </tr>
-                                        <tr>
-                        <td>Мощность																</td>
+                                                <tr>
+                                                        <td>Мощность</td>
                                                         <td>typ: 42 W; max: 48 W</td>
                                                 </tr>
-                                        <tr>
-                        <td>Мощность, макс.																</td>
+                                                <tr>
+                                                        <td>Мощность, макс.</td>
                                                         <td>48 W</td>
                                                 </tr>
-                                        <tr>
-                        <td>Напряжение питания																</td>
+                                                <tr>
+                                                        <td>Напряжение питания</td>
                                                         <td>12 V</td>
                                                 </tr>
-                                        <tr>
-                        <td>Ток потребления																</td>
+                                                <tr>
+                                                        <td>Ток потребления</td>
                                                         <td>typ: 4 A</td>
                                                 </tr>
-                                        <tr>
-                        <td>Длина																</td>
+                                                <tr>
+                                                        <td>Длина</td>
                                                         <td>5 000 мм</td>
                                                 </tr>
-                                        <tr>
-                        <td>Ширина																</td>
+                                                <tr>
+                                                        <td>Ширина</td>
                                                         <td>8 мм</td>
                                                 </tr>
-                                        <tr>
-                        <td>Высота																</td>
+                                                <tr>
+                                                        <td>Высота</td>
                                                         <td>1,8 мм</td>
                                                 </tr>
-                                        <tr>
-                        <td>Минимальный отрезок																</td>
+                                                <tr>
+                                                        <td>Минимальный отрезок</td>
                                                         <td>25 мм</td>
                                                 </tr>
-                                        <tr>
-                        <td>Сертификат																</td>
+                                                <tr>
+                                                        <td>Сертификат</td>
                                                         <td>РСТ-01461</td>
                                                 </tr>
-                                        <tr>
-                        <td>Вид упаковки																</td>
+                                                <tr>
+                                                        <td>Вид упаковки</td>
                                                         <td>Катушка</td>
                                                 </tr>
-                                        <tr>
-                        <td>Норма упаковки																</td>
+                                                <tr>
+                                                        <td>Норма упаковки</td>
                                                         <td>5</td>
                                                 </tr>
-                                        <tr>
-                        <td>Длина упаковки																</td>
+                                                <tr>
+                                                        <td>Длина упаковки</td>
                                                         <td>0.28</td>
                                                 </tr>
-                                        <tr>
-                        <td>Ширина упаковки																</td>
+                                                <tr>
+                                                        <td>Ширина упаковки</td>
                                                         <td>0.236</td>
                                                 </tr>
-                                        <tr>
-                        <td>Высота упаковки																</td>
+                                                <tr>
+                                                        <td>Высота упаковки</td>
                                                         <td>0.012</td>
                                                 </tr>
-                                        <tr>
-                        <td>Вес упаковки																</td>
+                                                <tr>
+                                                        <td>Вес упаковки</td>
                                                         <td>0.137</td>
                                                 </tr>
-                                        <tr>
-                        <td>Объём упаковки																</td>
+                                                <tr>
+                                                        <td>Объём упаковки</td>
                                                         <td>0.0008</td>
                                                 </tr>
-                                                                <tr>
-                        <td>
-                        Гарантийный срок							</td>
-                        <td>
-                        5 лет
-                        </td>
-                        </tr>						
-                                        <tr>
-                        <td>
-                        Производитель
-                        </td>
-                        <td>
-                                                                        <a href='https://arlight.ru ' rel='nofollow' target='_blank'>Arlight</a>
-                                                        </td>
-                        </tr>
-                        </tbody></table>
+                                                <tr>
+                                                        <td>Гарантийный срок</td>
+                                                        <td>5 лет</td>
+                                                </tr>						
+                                                <tr>
+                                                        <td>Производитель</td>
+                                                        <td><a href='https://arlight.ru ' rel='nofollow' target='_blank'>Arlight</a></td>
+                                                </tr>
+                                        </tbody>
+                                </table>
                         </div>";
 return $harakteristiki_def_1;                        
 
