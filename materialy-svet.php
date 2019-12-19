aad842b0d2731beacc09eaeaa444913f71a35c88
Шишенко Сергей <sshishenko@sd.net>
2019-12-19 10:30:03 +0400

    lenta_hot_white

--- materialy-svet.php
+++ materialy-svet.php
@@ -9,7 +9,7 @@ include 'inc/inc-header-menu3.php';
 include 'inc/inc-left-side-menu-materialy.php';
 
 if (isset($_GET["group"])) {
-    $article_id =$_GET["group"];// название группы и имя массива группы
+    $article_id =$_GET["group"];// название группы и имя массива группы lenta
 }else{
    $article_id ="svet";// название группы и имя массива группы 
 }
@@ -17,7 +17,7 @@ if (isset($_GET["group"])) {
 
 // $curr_page = 1;// индекс элемента в группе
 switch ($_GET["node"]) {
-    case "lenta":
+    case "lenta_hot_white":
         $curr_page = 1;
         break;
     case "profily":
@@ -38,13 +38,14 @@ switch ($_GET["node"]) {
 // $much_arr=[$$article_id];
 // $much_arr=[$lenta,$alumprofily,$transformatory,$accessories];
 $much_arr=[$$article_id];
+// $much_arr=[$lenta_hot_white];
 contentPage2($much_arr,$curr_page,$article_id); 
 
 
 // sideMenu($$article_id,$curr_page); 
 
 // $arrlist = [$$article_id];//группы в боковом меню
-$arrlist = [$lenta,$alumprofily,$transformatory,$accessories];//группы в боковом меню
+$arrlist = [$lenta_hot_white,$alumprofily,$transformatory,$accessories];//группы в боковом меню
 sideMenuListPages($arrlist);
 
 include 'inc/inc-footer2.php'; 
