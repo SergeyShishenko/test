0f1a821b5cdc7d1b6e6d29c0ff3c15e09a74a543
SergeyShishenko <serge-meb@mail.ru>
2020-03-16 23:04:46 +0300

    phpQuery

--- Classes/phpquery-master/demo2.php
+++ Classes/phpquery-master/demo2.php
@@ -1,4 +1,5 @@
 <?php
+// Classes\phpquery-master\demo2.php
 require('phpQuery/phpQuery.php');
 
 $habrablog = file_get_contents('https://www.mdm-complect.ru/catalog/sistema-joker-i-uno/60124/');
@@ -10,15 +11,23 @@ $arr=[];
 foreach ($hentry as $el) {
   $pq = pq($el)->find('h1')->text(); // Это аналог $ в jQuery
   $pq2 = pq($el)->find('p')->text(); // Это аналог $ в jQuery
-//   $pq = $pq->find('h1')->text(); // меняем атрибуты найденого элемента
-//   $pq->find('div.entry-info')->remove(); // удаляем ненужный элемент
-//   $tags = $pq->find('ul.tags > li > a');
-//   $tags->append(': ')->prepend(' :'); // добавляем двоеточия по бокам
-//   $pq->find('div.content')->prepend('<br />')->prepend($tags); // добавляем контент в начало найденого элемента
 }
+$hentry = $document->find('div#tab01');
+foreach ($hentry as $el) { 
+  $pq3 = pq($el)->find('p:first')->text(); // Это аналог $ в jQuery
+}
+$hentry = $document->find('div.f-val');
+$pq4 = pq($hentry)->find('span[itemprop="brand"]')->html(); // Это аналог $ в jQuery
+// foreach ($hentry as $el) { 
+//   $pq4 = pq($el)->find('span[itemprop="brand"]')->html(); // Это аналог $ в jQuery
+// }
 
 // echo $hentry;
 // var_dump($pq);
 echo $pq;
 echo '<br>';
 echo $pq2;
+echo '<br>';
+echo $pq3;
+echo '<br>';
+echo $pq4;
