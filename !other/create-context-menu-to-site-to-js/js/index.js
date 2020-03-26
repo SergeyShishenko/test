2cdb912790f0f46539e703f11479a8aded5eb83c
Шишенко Сергей <sshishenko@sd.net>
2020-03-26 15:20:05 +0400

    contextmenu

--- !other/create-context-menu-to-site-to-js/js/index.js
+++ !other/create-context-menu-to-site-to-js/js/index.js
@@ -8,14 +8,22 @@ function showMenu(x, y){
 }
 
 function hideMenu(){
-    menu.classList.remove('show-menu');
+    menu.classList.remove('show-menu');   
 }
 
 function onContextMenu(e){
     e.preventDefault();
+    wrapObj = document.querySelectorAll('.sel');
+
+    for(let i = 0;i<wrapObj.length;i++){
+      wrapObj[i].classList.remove('sel');
+    }
+
     showMenu(e.pageX, e.pageY);
     // document.addEventListener('mousedown', onMouseDown, false);
     console.log(e.target.closest('tr').id);
+    tr=e.target.closest('tr').classList.add('sel');
+    menu.classList.add('show-menu');
     document.getElementById('del').dataset.element=e.target.closest('tr').id;
 }
 
@@ -24,7 +32,11 @@ function onContextMenu(e){
 //     document.removeEventListener('mousedown', onMouseDown);
 // }
 
-
+function delSel(tr){
+    hideMenu(); 
+    document.getElementById(tr.dataset.element).classList.remove('sel');
+  
+}
 
 document.querySelectorAll('#tblVPI  tr')
 .forEach(e => e.addEventListener('contextmenu', onContextMenu, false)); 
