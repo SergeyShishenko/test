32a8fad574c09124b5db9239ef63f1d9ed85abb1
SergeyShishenko <serge-meb@mail.ru>
2020-03-26 22:54:55 +0300

    contextmenu

--- !other/create-context-menu-to-site-to-js/js/index.js
+++ !other/create-context-menu-to-site-to-js/js/index.js
@@ -8,20 +8,16 @@ function showMenu(x, y){
 }
 
 function hideMenu(){
-    menu.classList.remove('show-menu');   
+    menu.classList.remove('show-menu'); 
+    removeSel();  
 }
 
 function onContextMenu(e){
     e.preventDefault();
-    wrapObj = document.querySelectorAll('.sel');
-
-    for(let i = 0;i<wrapObj.length;i++){
-      wrapObj[i].classList.remove('sel');
-    }
-
+    removeSel();
     showMenu(e.pageX, e.pageY);
     // document.addEventListener('mousedown', onMouseDown, false);
-    console.log(e.target.closest('tr').id);
+    // console.log(e.target.closest('tr').id);
     tr=e.target.closest('tr').classList.add('sel');
     menu.classList.add('show-menu');
     document.getElementById('del').dataset.element=e.target.closest('tr').id;
@@ -34,9 +30,23 @@ function onContextMenu(e){
 
 function delSel(tr){
     hideMenu(); 
-    document.getElementById(tr.dataset.element).classList.remove('sel');
-  
+    document.getElementById(tr.dataset.element).classList.remove('sel'); 
+    del=document.getElementById('del').dataset.element;
+    // console.log(del);
+    ell=document.getElementById(del);
+    ell.parentElement.removeChild(ell);
+    // var ell = table.rows[j].cells[0].closest("tr"); // tr element (ваша строчка)
+    // document.getElementById(tr.dataset.element).parentElement.removeChild(tr.dataset.element); 
 }
 
-document.querySelectorAll('#tblVPI  tr')
+function removeSel(){
+   
+    wrapObj = document.querySelectorAll('.sel');
+
+    for(let i = 0;i<wrapObj.length;i++){
+      wrapObj[i].classList.remove('sel');
+    }
+}
+
+document.querySelectorAll('#tblVPI  tr:not(.head)')
 .forEach(e => e.addEventListener('contextmenu', onContextMenu, false)); 
