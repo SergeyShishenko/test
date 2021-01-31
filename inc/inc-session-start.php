f54cb529d03d5ebe775c8e44b9e3b40c29368e3d
SergeyShishenko <serge-meb@mail.ru>
2021-01-31 12:08:55 +0300

    cnc

--- inc/inc-session-start.php
+++ inc/inc-session-start.php
@@ -6,9 +6,25 @@ session_start();
 // $s_id=session_id();
 
 $ref = $_SERVER['REQUEST_URI'];
+// echo basename($ref);
 $_SESSION['ref'] = $ref;
-define('__ROOT__', dirname(dirname(dirname(__FILE__)))); 
+
+define('__ROOT__', dirname(dirname(dirname(__FILE__)))); // было
+// require_once(__ROOT__.'/DATA/TABLES/configDB.php'); // подключение к базе данных
+
+// define('__ROOT__', $_SERVER['DOCUMENT_ROOT']); 
 require_once(__ROOT__.'/DATA/TABLES/configDB.php'); // подключение к базе данных
+
+// echo __ROOT__ ,PHP_EOL;
+//  echo $_SERVER['DOCUMENT_ROOT'],PHP_EOL;
+// echo basename($_SERVER['DOCUMENT_ROOT']);
+// /var/www/u0474172/data/www
+// /var/www/u0474172/data/www/500stp.website
+// $loc = 'Location:'.$_SERVER['DOCUMENT_ROOT'].'/www/index-session.php';
+
+    
+
+
 $dbconn=dbconnect();
 // проверка сессии
 $hash=$_COOKIE["hash"];
@@ -29,7 +45,13 @@ $result = mysqli_query($dbconn,"SELECT * FROM `sofia_users` WHERE `user_hash` LI
 
     if (mysqli_num_rows($result) == 0) {//есть запись     
 
-        if(!isset($_SESSION['sess_login']) || !isset($_SESSION['sess_pass']) ) { header('Location:index-session.php');    exit();}
+        if(!isset($_SESSION['sess_login']) || !isset($_SESSION['sess_pass']) ) {
+            if (basename($ref)=="CNC"){
+                header('Location:../index-session.php');    exit();
+            }
+             header('Location:index-session.php');    exit();
+            }
+        // if(!isset($_SESSION['sess_login']) || !isset($_SESSION['sess_pass']) ) { header($loc);    exit();}
 
     }
    
