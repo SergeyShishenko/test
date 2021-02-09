ca202ad7a4a1d22100fcd38b07f39e840f0eb43b
Шишенко Сергей <sshishenko@sd.net>
2021-02-09 10:08:44 +0400

    SVG

--- !other/SVG/index.php
+++ !other/SVG/index.php
@@ -15,25 +15,101 @@
 
 </head>
 <body>
-    <svg width="400" height="400" xmlns="http://www.w3.org/2000/svg">
-    <rect x="0" y="0" width="400" height="400" 
-          fill="none" stroke="black" stroke-width="5px" stroke-opacity="0.5"/>
+<!-- 1 -->
+        <svg width="400" height="400" xmlns="http://www.w3.org/2000/svg">
+        <rect x="0" y="0" width="400" height="400" 
+            fill="none" stroke="black" stroke-width="5px" stroke-opacity="0.5"/>
 
-    <path d="M10 10"/>
+        <path d="M10 10"/>
 
-    <!-- Точки -->
-    <circle cx="200" cy="200" r="5" fill="red"/>
-    <!-- <g fill-opacity="0.6" stroke="black" stroke-width="0.5px" >
-        <circle cx="200px" cy="200px" r="104px" fill="red"   transform="translate(  0,-52)" />
-        <circle cx="200px" cy="200px" r="104px" fill="blue"  transform="translate( 60, 52)" />
-        <circle cx="200px" cy="200px" r="104px" fill="green" transform="translate(-60, 52)" />
-     </g> -->
-    <g fill-opacity="0.6" stroke="black" stroke-width="0.5px"  >
-        <circle class="hover" cx="200px" cy="200px" r="104px" fill="red"   transform="translate(  0,-52)" />
-        <circle class="hover" cx="200px" cy="200px" r="104px" fill="blue"  transform="translate( 60, 52)" />
-        <circle class="hover" cx="200px" cy="200px" r="104px" fill="green" transform="translate(-60, 52)" />
-     </g>
+        <!-- Точки -->
+        <circle cx="200" cy="200" r="5" fill="red"/>
+        <!-- <g fill-opacity="0.6" stroke="black" stroke-width="0.5px" >
+            <circle cx="200px" cy="200px" r="104px" fill="red"   transform="translate(  0,-52)" />
+            <circle cx="200px" cy="200px" r="104px" fill="blue"  transform="translate( 60, 52)" />
+            <circle cx="200px" cy="200px" r="104px" fill="green" transform="translate(-60, 52)" />
+        </g> -->
+        <g fill-opacity="0.6" stroke="black" stroke-width="0.5px"  >
+            <circle class="hover" cx="200px" cy="200px" r="104px" fill="red"   transform="translate(  0,-52)" />
+            <circle class="hover" cx="200px" cy="200px" r="104px" fill="blue"  transform="translate( 60, 52)" />
+            <circle class="hover" cx="200px" cy="200px" r="104px" fill="green" transform="translate(-60, 52)" />
+        </g>
 
-    </svg>
+        </svg>
+
+<br>
+<!-- 2 -->
+<!-- https://svg-art.ru/?p=5 -->
+
+        <svg width="400" height="400" xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 400 400" preserveAspectRatio="none"            
+            style="border: 2px dashed blue;">
+            
+            <!--colored circles in corners of  the canvas  -->
+            <circle cx="55" cy="55" r="50" fill="rgba(20,255,24, 0.5)"  stroke="rgba(6,90,6, 0.75)" stroke-width="2"/>
+            <circle cx="350" cy="55" r="50" fill="rgba(57,65,212, 0.5)"  stroke="rgba(26,31,167, 0.75)" stroke-width="2"/>
+            <circle cx="350" cy="350" r="50" fill="rgba(212,30,75, 0.5)"  stroke="rgba(212,30,75, 0.75)" stroke-width="2"/>
+            <circle cx="55" cy="350" r="50" fill="rgba(255,221,0, 0.5)"  stroke="rgba(169,146,0, 0.75)" stroke-width="2"/>
+        
+            <!--the red rectangle outside viewing area  svg   -->
+            <rect x="390" y="165" width="100" height="100" fill="rgba(244,47,39, 0.6)"  stroke="rgba(244,47,39, 0.75)" stroke-width="2"/>
+        </svg>
+        <!-- Смещение начала координат viewBox по одной оси -->
+        <svg width="400" height="400" xmlns="http://www.w3.org/2000/svg"
+            viewBox="70 0 400 400" preserveAspectRatio="none"            
+            style="border: 2px dashed blue;">
+            
+            <!--colored circles in corners of  the canvas  -->
+            <circle cx="55" cy="55" r="50" fill="rgba(20,255,24, 0.5)"  stroke="rgba(6,90,6, 0.75)" stroke-width="2"/>
+            <circle cx="350" cy="55" r="50" fill="rgba(57,65,212, 0.5)"  stroke="rgba(26,31,167, 0.75)" stroke-width="2"/>
+            <circle cx="350" cy="350" r="50" fill="rgba(212,30,75, 0.5)"  stroke="rgba(212,30,75, 0.75)" stroke-width="2"/>
+            <circle cx="55" cy="350" r="50" fill="rgba(255,221,0, 0.5)"  stroke="rgba(169,146,0, 0.75)" stroke-width="2"/>
+        
+            <!--the red rectangle outside viewing area  svg   -->
+            <rect x="390" y="165" width="100" height="100" fill="rgba(244,47,39, 0.6)"  stroke="rgba(244,47,39, 0.75)" stroke-width="2"/>
+        </svg>
+        <!-- Смещение начала координат viewBox по двум осям -->
+        <svg width="400" height="400" xmlns="http://www.w3.org/2000/svg"
+            viewBox="70 70 400 400" preserveAspectRatio="none"            
+            style="border: 2px dashed blue;">
+            
+            <!--colored circles in corners of  the canvas  -->
+            <circle cx="55" cy="55" r="50" fill="rgba(20,255,24, 0.5)"  stroke="rgba(6,90,6, 0.75)" stroke-width="2"/>
+            <circle cx="350" cy="55" r="50" fill="rgba(57,65,212, 0.5)"  stroke="rgba(26,31,167, 0.75)" stroke-width="2"/>
+            <circle cx="350" cy="350" r="50" fill="rgba(212,30,75, 0.5)"  stroke="rgba(212,30,75, 0.75)" stroke-width="2"/>
+            <circle cx="55" cy="350" r="50" fill="rgba(255,221,0, 0.5)"  stroke="rgba(169,146,0, 0.75)" stroke-width="2"/>
+        
+            <!--the red rectangle outside viewing area  svg   -->
+            <rect x="390" y="165" width="100" height="100" fill="rgba(244,47,39, 0.6)"  stroke="rgba(244,47,39, 0.75)" stroke-width="2"/>
+        </svg>
+        <BR>
+        <!-- Увеличение масштаба М2:1 -->
+        <svg width="400" height="400" xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 200 200" preserveAspectRatio="none"            
+            style="border: 2px dashed blue;">
+            
+            <!--colored circles in corners of  the canvas  -->
+            <circle cx="55" cy="55" r="50" fill="rgba(20,255,24, 0.5)"  stroke="rgba(6,90,6, 0.75)" stroke-width="2"/>
+            <circle cx="350" cy="55" r="50" fill="rgba(57,65,212, 0.5)"  stroke="rgba(26,31,167, 0.75)" stroke-width="2"/>
+            <circle cx="350" cy="350" r="50" fill="rgba(212,30,75, 0.5)"  stroke="rgba(212,30,75, 0.75)" stroke-width="2"/>
+            <circle cx="55" cy="350" r="50" fill="rgba(255,221,0, 0.5)"  stroke="rgba(169,146,0, 0.75)" stroke-width="2"/>
+        
+            <!--the red rectangle outside viewing area  svg   -->
+            <rect x="390" y="165" width="100" height="100" fill="rgba(244,47,39, 0.6)"  stroke="rgba(244,47,39, 0.75)" stroke-width="2"/>
+        </svg>
+        <!-- Уменьшение масштаба М1:2 -->
+        <svg width="400" height="400" xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 800 800" preserveAspectRatio="none"            
+            style="border: 2px dashed blue;">
+            
+            <!--colored circles in corners of  the canvas  -->
+            <circle cx="55" cy="55" r="50" fill="rgba(20,255,24, 0.5)"  stroke="rgba(6,90,6, 0.75)" stroke-width="2"/>
+            <circle cx="350" cy="55" r="50" fill="rgba(57,65,212, 0.5)"  stroke="rgba(26,31,167, 0.75)" stroke-width="2"/>
+            <circle cx="350" cy="350" r="50" fill="rgba(212,30,75, 0.5)"  stroke="rgba(212,30,75, 0.75)" stroke-width="2"/>
+            <circle cx="55" cy="350" r="50" fill="rgba(255,221,0, 0.5)"  stroke="rgba(169,146,0, 0.75)" stroke-width="2"/>
+        
+            <!--the red rectangle outside viewing area  svg   -->
+            <rect x="390" y="165" width="100" height="100" fill="rgba(244,47,39, 0.6)"  stroke="rgba(244,47,39, 0.75)" stroke-width="2"/>
+        </svg>
 </body>
 </html>
