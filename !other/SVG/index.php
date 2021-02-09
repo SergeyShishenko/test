53579906f38af5a51eea4121e9537b12d62eb682
SergeyShishenko <serge-meb@mail.ru>
2021-02-09 21:59:24 +0300

    svg

--- !other/SVG/index.php
+++ !other/SVG/index.php
@@ -2,11 +2,12 @@
 // https://developer.mozilla.org/ru/docs/Web/SVG/Tutorial/Paths
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>SVG</title>
+    <link rel="shortcut icon" href="svg.ico" type="image/x-icon">  
     <style>
     .hover:hover{
         stroke:aqua; 
