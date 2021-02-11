d8e1e6790fb0b52857fe4b37dee0cd5678c10de7
Шишенко Сергей <sshishenko@sd.net>
2021-02-11 15:12:13 +0400

    csv

--- !other/CNC/check_specification_CSV.php
+++ !other/CNC/check_specification_CSV.php
@@ -1,27 +1,41 @@
+<pre>
 <?php
 // !other\CNC\check_specification_CSV.php
 $row = 1;
+// $arrstr[];
+// $arrAllCSV[];
 if (($handle = fopen("test.csv", "r")) !== FALSE) {
-    while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {        
+    while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {  // строка  до 1000 символов    
         if ($row > 1){
-            echo "<br>Строка $row: -> ";
-            $num = count($data);        
+            // echo "<br>Строка $row: -> ";
+            // $num = count($data);        
             // for ($c=0; $c < $num; $c++) {
             //     echo "_" . $data[$c] ;
             // } 
-            echo  " (№ Заказ: ". $data[0].") " ;          
-            echo  " (№ Изделие ". $data[1].") " ;          
-            echo  " (№ СЕ/пСЕ ". $data[2] .") ";          
-            echo  " (Деталь ". $data[3].") " ;          
-            echo  " (Длина ". $data[4] .") ";          
-            echo  " (Ширина ". str_replace(",", ".", $data[5]).") " ;          
-            echo  " (Толщина ". $data[6] .") "; 
-            if (str_replace(",", ".", $data[5]) == 1673.5) {
-                echo  " (Равно ". $data[5] .") ";
-            }         
+
+            $data[0]=str_replace(",", ".", $data[0]);// № Заказа
+            $data[1]=str_replace(",", ".", $data[1]);// № Изделия
+            $data[2]=str_replace(",", ".", $data[2]);// № СЕ/пСЕ
+            $data[3]=str_replace(",", ".", $data[3]);// № Детали
+            $data[4]=str_replace(",", ".", $data[4]);// № Детали
+            $data[5]=str_replace(",", ".", $data[5]);// № Детали
+            $data[6]=str_replace(",", ".", $data[6]);// № Детали
+            // echo  " (№ Заказа: ". $order.") " ;          
+            // echo  " (№ Изделия: ". $product.") " ;          
+            // echo  " (№ СЕ/пСЕ: ". $SE .") ";          
+            // echo  " (№ Детали: ". $numpart.") " ;          
+            // echo  " (Длина: ". $DL .") ";          
+            // echo  " (Ширина: ". $DH.") " ;          
+            // echo  " (Толщина: ". $DS .") "; 
+            // if (str_replace(",", ".", $data[5]) == 1673.5) {
+            //     echo  " (Равно ". $data[5] .") ";
+            // }
+            $arrAllCSV[]=$data;         
        }
        $row++;
     }
     fclose($handle);
+    var_dump($arrAllCSV);
 }
 ?>
+</pre>
