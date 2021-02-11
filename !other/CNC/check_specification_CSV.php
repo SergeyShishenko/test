2a182a8501ed821fc55ecaa8270f9cc2ebb15644
Шишенко Сергей <sshishenko@sd.net>
2021-02-11 10:00:08 +0400

    csv

--- !other/CNC/check_specification_CSV.php
+++ !other/CNC/check_specification_CSV.php
@@ -4,11 +4,21 @@ $row = 1;
 if (($handle = fopen("test.csv", "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {        
         if ($row > 1){
-            echo "<p> Строка $row: <br /></p>\n";
+            echo "<br>Строка $row: -> ";
             $num = count($data);        
-            for ($c=0; $c < $num; $c++) {
-                echo $data[$c] . "<br />\n";
-            }            
+            // for ($c=0; $c < $num; $c++) {
+            //     echo "_" . $data[$c] ;
+            // } 
+            echo  " (№ Заказ: ". $data[0].") " ;          
+            echo  " (№ Изделие ". $data[1].") " ;          
+            echo  " (№ СЕ/пСЕ ". $data[2] .") ";          
+            echo  " (Деталь ". $data[3].") " ;          
+            echo  " (Длина ". $data[4] .") ";          
+            echo  " (Ширина ". str_replace(",", ".", $data[5]).") " ;          
+            echo  " (Толщина ". $data[6] .") "; 
+            if (str_replace(",", ".", $data[5]) == 1673.5) {
+                echo  " (Равно ". $data[5] .") ";
+            }         
        }
        $row++;
     }
