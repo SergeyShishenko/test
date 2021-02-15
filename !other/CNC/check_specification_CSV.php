c18806ebbe33932ab80a1397c9216407d1bd1e58
Шишенко Сергей <sshishenko@sd.net>
2021-02-15 09:58:47 +0400

    specification

--- !other/CNC/check_specification_CSV.php
+++ !other/CNC/check_specification_CSV.php
@@ -13,33 +13,41 @@
 
 // !other\CNC\check_specification_CSV.php
 
+// $pathFile='files/1';
 $pathFile='files/2578_4_SE';
+// $pathFile='files/2578_4_numpart';
 $arrAllCSV[] = include "specificationCSV.php";
-$arrCHPU[] = include "specificationCHPU.php";
+// echo count($arrAllCSV[0])."<br>";
+// var_dump($arrAllCSV);
+if (count($arrAllCSV[0])>0){
+    $arrCHPU[] = include "specificationCHPU.php";
 
-foreach ($arrCHPU[0] as $var2){
-    $div_count= substr_count($var2['name'], '_');// количество "_"
-    $find=false;
-    foreach ($arrAllCSV[0] as $str) {  
-        if ($div_count==1){
-          $var1=$str[2]."_".$str[3];  
-        }elseif($div_count===0){
-            $var1=$str[3];  
+    foreach ($arrCHPU[0] as $var2){
+        $div_count= substr_count($var2['name'], '_');// количество "_"
+        $find=false;
+        foreach ($arrAllCSV[0] as $str) {  
+            if ($div_count==1){
+            $var1=$str[2]."_".$str[3];  
+            }elseif($div_count===0){
+                $var1=$str[3];  
+            }
+            $varSTR4=$str[4]." x ".$str[5]." x ".$str[6];
+            $var3=$var2['DL'] ." x ".$var2['DH']." x ".$var2['DS'];
+            
+            if (strcasecmp($var1, $var2['name']) == 0) {
+            echo "$var1 ok! ". $var3. ' AND '.$varSTR4;
+            if (strcasecmp($var3, $varSTR4) == 0) {
+                echo "<span style='color:green'> - Размеры совпадают" . "</span> <br>";
+            }else{
+                echo "<span style='color:red'> - Размеры неверные" . "</span> <br>";
+            }
+            $find=true;
+            }
         }
-        $varSTR4=$str[4]." x ".$str[5]." x ".$str[6];
-        $var3=$var2['DL'] ." x ".$var2['DH']." x ".$var2['DS'];
-        
-        if (strcasecmp($var1, $var2['name']) == 0) {
-        echo "$var1 ok! ". $var3. ' AND '.$varSTR4;
-        if (strcasecmp($var3, $varSTR4) == 0) {
-            echo "<span style='color:green'> - Размеры совпадают" . "</span> <br>";
-        }else{
-            echo "<span style='color:red'> - Размеры неверные" . "</span> <br>";
-        }
-        $find=true;
-        }
+        if (!$find){ echo $var2['name'] . "<span style='color:blue'> нет в спецификации </span> <br>";}
     }
-    if (!$find){ echo $var2['name'] . "<span style='color:blue'> нет в спецификации </span> <br>";}
+}else{
+    echo "Нет файла спецификации!";
 }
 
 ?>
--- !other/CNC/specificationCSV.php
+++ !other/CNC/specificationCSV.php
@@ -1,5 +1,10 @@
 <?php 
 include "classes_cnc/class_CSVspecification.php";
-$scsv= new class_CSVspecification($pathFile);
-return $scsv->getSpecification();
+if (file_exists($pathFile)) {
+    $scsv= new class_CSVspecification($pathFile);
+    return $scsv->getSpecification();
+}else{
+    return false;
+}
+
 ?>
