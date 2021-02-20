baca96e1ce5878f2bfe4010c7eed1f090858a0d7
Шишенко Сергей <sshishenko@sd.net>
2021-02-20 11:20:35 +0400

    stl

--- !other/STL/ReadSTL.php
+++ !other/STL/ReadSTL.php
@@ -13,10 +13,10 @@
 // Каждый треугольник представлен двенадцатью 32-битными числами с плавающей запятой. Как и в файле ASCII STL, 3 числа относятся к трехмерным декартовым координатам нормали к треугольнику. Остальные 9 чисел предназначены для координат вершин (по три в каждой). Вот как это выглядит:
 
 // foreach triangle
-// REAL32[3] – Нормальный вектор
-// REAL32[3] – Вершина 1
-// REAL32[3] – Вершина 2
-// REAL32[3] – Вершина 3
+// REAL32[2] – Нормальный вектор
+// REAL32[2] – Вершина 1
+// REAL32[2] – Вершина 2
+// REAL32[2] – Вершина 3
 // UINT16 – конец подсчета байтов аттрибута
 // Обратите внимание, что после каждого треугольника есть 2-байтовая последовательность, называемая «счетчиком байтов атрибута». В большинстве случаев это значение равно нулю и действует как разделитель между двумя треугольниками. Но некоторые программы также используют эти 2 байта для кодирования дополнительной информации о треугольнике. Мы увидим такой пример позже, где эти байты будут использоваться для хранения информации о цвете.
 //*
@@ -34,8 +34,8 @@
 // Бинарный STL-файл условно можно изобразить следующим образом:
 // struct facet
 // {
-//     float normal[3];     // 12 байт
-//     float vertex[3][3];  // 36 байт
+//     float normal[2];     // 12 байт
+//     float vertex[2][2];  // 36 байт
 //     unsigned short attr; // 2 байта
 // };
 
@@ -49,299 +49,122 @@
 
 // $filename = "C:/xampp/tmp/" .basename($_FILES['uploadfile']['tmp_name']);
  $filename = "box.STL";
+ $sum=0;
 
-//  $handle = fopen($filename, "rb");
+// $handle = fopen($filename, "rb");
 // $contents = fread($handle, filesize($filename));
 // fclose($handle);
-// // var_dump(unpack('f', "\x99\x99\x19\x3f"));
+// // var_dump(unpack('f', "\x99\x99\$arrVert1[0]9\$arrVert3[0]f"));
 // // var_dump(unpack('f', $contents));
 // var_dump($contents);
 
-// $emptyArray = array();
-// $normal = array();
-// $vertex1 = array();
-// $vertex2 = array();
-// $vertex3 = array();
-// $count = 3;
 $handle = fopen($filename, "rb");
 $name = fread($handle, 80);
 echo "Заголовок: ".$name;
 $content = unpack('i', fread($handle, 4));
-echo "<br> Треугольников: ".$content[1] . "<br>";
+echo "<br> Всего треугольников: ".$content[1] . "<br>";
+echo "<hr>";
+  for ($j = 1; $j <= $content[1]; $j++){
+    echo "Треугольник - $j<br>"; 
+    $NormalX=unpack('f', fread($handle, 4))[1];
+    $NormalY=unpack('f', fread($handle, 4))[1];
+    $NormalZ=unpack('f', fread($handle, 4))[1];
+    echo "Нормаль: " . $NormalX . " " . $NormalY . " " . $NormalZ . "<br>";
+
+    $Vertex_1_X=unpack('f', fread($handle, 4))[1];
+    $Vertex_1_Y=unpack('f', fread($handle, 4))[1];
+    $Vertex_1_Z=unpack('f', fread($handle, 4))[1];
+    $arrVert1=[$Vertex_1_X, $Vertex_1_Y, $Vertex_1_Z];
+    echo "Вершина1: " . $Vertex_1_X . " " . $Vertex_1_Y. " " . $Vertex_1_Z. "<br>";
+
+    $Vertex_2_X=unpack('f', fread($handle, 4))[1];
+    $Vertex_2_Y=unpack('f', fread($handle, 4))[1];
+    $Vertex_2_Z=unpack('f', fread($handle, 4))[1];
+    $arrVert2=[$Vertex_2_X ,$Vertex_2_Y, $Vertex_2_Z];
+    echo "Вершина2: " . $Vertex_2_X . " " . $Vertex_2_Y. " " . $Vertex_2_Z. "<br>";
+
+    $Vertex_3_X=unpack('f', fread($handle, 4))[1];
+    $Vertex_3_Y=unpack('f', fread($handle, 4))[1];
+    $Vertex_3_Z=unpack('f', fread($handle, 4))[1];
+    $arrVert3=[$Vertex_3_X, $Vertex_3_Y, $Vertex_3_Z];
+    echo "Вершина3: " . $Vertex_3_X . " " . $Vertex_3_Y. " " . $Vertex_3_Z. "<br>";
+    
+    unpack('s', fread($handle, 2))[1]; // END
+    // echo unpack('s', fread($handle, 2))[1]. "<br>";
+    $strain=square($arrVert1,$arrVert2,$arrVert3);
+    echo "S = " . $strain;
+    $sum += $strain;
+    
+    echo "<hr>";
+    }
+
+    echo "S-Solid = " .$sum;
+// Для упрощения расчетов введите вспомогательную переменную - полупериметр (Р).
+// Из названия понятно, что это половина суммы длин всех сторон:
+// Р = ½*(AB+BC+AC) = 0.5*(√((X₁-X₂)² + (Y₁-Y₂)² + (Z₁-Z₂)²) + √((X₂-X₃)² + (Y₂-Y₃)² + (Z₂-Z₃)²) + √((X₁-X₃)² + (Y₁-Y₃)² + (Z₁-Z₃)²)
+// vertex_1 2.22934 -0.992723 -0.862826  X_1 Y_1 Z_1 $arrVert1[0] $arrVert1[1] $arrVert1[2]
+// vertex_2 2.41037 -0.777999 -0.841105  X_2 Y_2 Z_2 $arrVert2[0] $arrVert2[1] $arrVert2[2]
+// vertex_3 2.40731 -0.97498 -0.805091   X_3 Y_3 Z_3 $arrVert3[0] $arrVert3[1] $arrVert3[2]
+function square($arrVert1,$arrVert2,$arrVert3){ // полупериметр
+//  Р = ½*(AB+BC+AC) = 
+//  ½*(√((X₁-X₂)² + 
+//  (Y₁-Y₂)² + 
+//  (Z₁-Z₂)²) + 
+//  √((X₂-X₃)² + 
+//  (Y₂-Y₃)² + 
+//  (Z₂-Z₃)²) + 
+//  √((X₁-X₃)² + 
+//  (Y₁-Y₃)² + 
+//  (Z₁-Z₃)²)
+    $halfMeter= 0.5 * (sqrt(pow($arrVert1[0] - $arrVert2[0],2) + pow($arrVert1[1] - $arrVert2[1],2) +  pow($arrVert1[2] - $arrVert2[2],2)) +  
+                       sqrt(pow($arrVert2[0] - $arrVert3[0],2) + pow($arrVert2[1] - $arrVert3[1],2) +  pow($arrVert2[2] - $arrVert3[2],2)) +  
+                       sqrt(pow($arrVert1[0] - $arrVert3[0],2) + pow($arrVert1[1] - $arrVert3[1],2) +  pow($arrVert1[2] - $arrVert3[2],2))
+                        );
+                        
+
+// √( P*(P-√((X₁-X₂)² +                 (Y₁-Y₂)² +                          (Z₁-Z₂)²))*
+//      (P-√((X₂-X₃)² +                 (Y₂-Y₃)² +                          (Z₂-Z₃)²))*
+//      (P-√((X₁-X₃)² +                 (Y₁-Y₃)² +                          (Z₁-Z₃)²)
+
+    $square = sqrt( $halfMeter * ($halfMeter-sqrt(pow($arrVert1[0]-$arrVert2[0],2) + pow($arrVert1[1]-$arrVert2[1],2) + pow($arrVert1[2]-$arrVert2[2],2))) * 
+                                 ($halfMeter-sqrt(pow($arrVert2[0]-$arrVert3[0],2) + pow($arrVert2[1]-$arrVert3[1],2) + pow($arrVert2[2]-$arrVert3[2],2))) * 
+                                 ($halfMeter-sqrt(pow($arrVert1[0]-$arrVert3[0],2) + pow($arrVert1[1]-$arrVert3[1],2) + pow($arrVert1[2]-$arrVert3[2],2)))
+                );
+    return $square ;
+}
+
+// Рассчитайте площадь (S) по формуле Герона - извлеките корень из произведения полупериметра на разность между ним и длиной каждой из сторон. 
+// В общем виде ее можно записать так: 
+// S = √(P*(P-AB)*(P-BC)*(P-AC)) = 
+// sqrt( P*(P-sqrt(pow($arrVert1[0]-$arrVert2[0],2) + 
+// pow($arrVert1[1]-$arrVert2[1],2) + 
+// pow($arrVert1[2]-$arrVert2[2],2)))*(P-sqrt(pow($arrVert2[0]-$arrVert3[0],2) +
+// pow($arrVert2[1]-$arrVert3[1],2) + 
+// pow($arrVert2[2]-$arrVert3[2],2)))*(P-sqrt(pow($arrVert1[0]-$arrVert3[0],2) +
+// pow($arrVert1[1]-$arrVert3[1],2) +
+// pow($arrVert1[2]-$arrVert3[2],2)))
+
+// Для упрощения расчетов введите вспомогательную переменную - полупериметр (Р). Из названия понятно, что это половина суммы длин всех сторон:
+//  Р = ½*(AB+BC+AC) = 
+//  ½*(√((X₁-X₂)² + 
+//  (Y₁-Y₂)² + 
+//  (Z₁-Z₂)²) + 
+//  √((X₂-X₃)² + 
+//  (Y₂-Y₃)² + 
+//  (Z₂-Z₃)²) + 
+//  √((X₁-X₃)² + 
+//  (Y₁-Y₃)² + 
+//  (Z₁-Z₃)²).
+
+// Рассчитайте площадь (S) по формуле Герона - извлеките корень из произведения полупериметра на разность между ним и длиной каждой из сторон. В общем виде ее можно записать так: S = √(P*(P-AB)*(P-BC)*(P-AC)) = 
+// √( P*(P-√((X₁-X₂)² 
+// + (Y₁-Y₂)² + 
+// (Z₁-Z₂)²))*(P-√((X₂-X₃)² + 
+// (Y₂-Y₃)² + 
+// (Z₂-Z₃)²))*(P-√((X₁-X₃)² + 
+// (Y₁-Y₃)² + 
+// (Z₁-Z₃)²)).
 
 
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('s', fread($handle, 2))[1]. "<br>";
-echo "///***///<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('s', fread($handle, 2))[1]. "<br>";
-echo "///***///<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('s', fread($handle, 2))[1]. "<br>";
-echo "///***///<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('s', fread($handle, 2))[1]. "<br>";
-echo "///***///<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('s', fread($handle, 2))[1]. "<br>";
-echo "///***///<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('s', fread($handle, 2))[1]. "<br>";
-echo "///***///<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('s', fread($handle, 2))[1]. "<br>";
-echo "///***///<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('s', fread($handle, 2))[1]. "<br>";
-echo "///***///<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('s', fread($handle, 2))[1]. "<br>";
-echo "///***///<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('s', fread($handle, 2))[1]. "<br>";
-echo "///***///<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('s', fread($handle, 2))[1]. "<br>";
-echo "///***///<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo unpack('f', fread($handle, 4))[1]. "<br>";
-echo "***<br>";
-echo unpack('s', fread($handle, 2))[1]. "<br>";
-echo "///***///<br>";
-
-
-
-// echo filesize($filename);
-// fseek($handle,80);
-// while (feof($handle)==false) {
-//     // for ($j = 1; $j <= 4; $j++){
-//             $contents = fread($handle, 50);
-//             $data[] = unpack('f', $contents);
-//     // }
-//     echo "<pre>";
-// echo "***";
-// var_dump($data);
-// echo "***";
-// echo "</pre>";
-
-// }
-// echo "<pre>";
-// echo "***";
-// var_dump($normal);
-// echo "***";
-// echo "</pre>";
-
-// print("<br>");
-
-// for ($e = 1; $e <= 3; $e++){
-//         $content = fread($handle, 4);
-//         $vertex1[] = (float)$content;
-// }
-
-// var_dump($vertex1);
-// print("<br>");
-
-// for ($d = 1; $d <= 3; $d++){
-//         $contents = fread($handle, 4);
-//         $vertex2[] = (float)$content;
-// }
-
-// var_dump($vertex2);
-// print("<br>");
-
-// for ($f = 1; $f <= 3; $f++){
-//         $contents = fread($handle, 4);
-//         $vertex3[] = (float)$content;
-// }
-
-// var_dump($vertex3);
-// print("<br>");
-
-// fseek($handle,2);
-// $sum = $sum + ((((-1) * current($vertex3) * next($vertex2) * end($vertex1)) + prev($vertex2) * next($vertex3) * prev($vertex1) + prev($vertex3) * current($vertex1) * end($vertex2) - prev($vertex1) * next($vertex3) * current($vertex2) - reset($vertex2) * next($vertex1) * next($vertex3) + prev($vertex1) * next($vertex2) * current($vertex3)) / 6);
-// $normal = array();
-// $vertex1 = array();
-// $vertex2 = array();
-// $vertex3 = array();
-// } 
-//  fclose($handle);
-// print($sum);
+// Подробнее: https://www.kakprosto.ru/kak-124513-kak-nayti-ploshchad-treugolnika-po-trem-tochkam#ixzz6myrXMJFt
 ?>
