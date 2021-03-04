bc1977bb5d870bd3fe7fc92d66ca746190d2dff2
SergeyShishenko <serge-meb@mail.ru>
2021-03-04 21:21:05 +0300

    scene

--- !other/THREEJS/STLloader/js/script.js
+++ !other/THREEJS/STLloader/js/script.js
@@ -1,3 +1,4 @@
+// https://threejsfundamentals.org/threejs/lessons/ru/threejs-cameras.html
 var container, camera, scene, renderer;
 var cube, mesh;
 var controls;
@@ -7,9 +8,10 @@ animate();
 
 function init(){
 
-  container = document.createElement("div");
-  document.body.appendChild(container);
+  // container = document.createElement("div");
+  // document.body.appendChild(container);
 
+  var container = document.getElementById('canvas');
     renderer = new THREE.WebGLRenderer( {antialias:true} );
     var width = window.innerWidth;
     var height = window.innerHeight;
@@ -26,11 +28,13 @@ function init(){
     // scene.add (cube);
     var loader = new THREE.STLLoader();
     // var stl="DW-1274(800)-010R.stl";
-    var stl="DW-1274(800)-009.stl";
-    // var stl="box.stl";
+    // var stl="DW-1274(800)-009.stl";
+    var stl="box.stl";
+    // var stl="DW-1270-007R.stl";
     loader.load(stl, function(geometry) {
       // material = new THREE.MeshPhongMaterial({ color: 0xff5533 });
-      material = new THREE.MeshBasicMaterial ({color: 0x1ec876});
+      // material = new THREE.MeshBasicMaterial ({color: 0x1ec876});
+      material = new THREE.MeshNormalMaterial();
   
       mesh = new THREE.Mesh(geometry, material);
       // mesh.scale.set(0.01,0.01,0.01);
@@ -39,19 +43,36 @@ function init(){
       scene.add(mesh);
       renderer.render(scene, camera);
     });
+
+    	// lights
+
+      // var light = new THREE.DirectionalLight( 0xffffff );
+      // light.position.set( 10, 10, 10 );
+      // scene.add( light );
+
+      // light = new THREE.DirectionalLight( 0x002288 );
+      // light.position.set( -10, -10, -10 );
+      // scene.add( light );
+
+      // light = new THREE.AmbientLight( 0x222222 );
+      // scene.add( light );
+
+      //
       // renderer.render(scene, camera);
 
-    camera = new THREE.PerspectiveCamera (45, width/height, 0.1, 10000);
+    camera = new THREE.PerspectiveCamera (45, width/height, 0.1, 1000);
+    camera.position.y = 400;
+    camera.position.x = 400;
     // camera.position.y = 160;
-    // camera.position.z = 400;
-    camera.position.y = 160;
-    camera.position.z = 400;
+    camera.position.z = -400;
+    // camera.up.set(0, 0, -1);
+    
     camera.lookAt (new THREE.Vector3(0,0,0));
 
     controls = new THREE.OrbitControls (camera, renderer.domElement);
     
-    var gridXZ = new THREE.GridHelper(100, 100);
-    gridXZ.setColors( new THREE.Color(0xff0000), new THREE.Color(0xffffff) );
+    var gridXZ = new THREE.GridHelper(100, 10);
+    // gridXZ.setColors( new THREE.Color(0xff0000), new THREE.Color(0xffffff) );
     scene.add(gridXZ);
 
 }
@@ -69,4 +90,5 @@ controls = new THREE.OrbitControls( camera, renderer.domElement );
 controls.enableDamping = true;
 controls.dampingFactor = 0.25;
 controls.enableZoom = true;
-controls.autoRotate = true;
+// controls.autoRotate = true;
+controls.autoRotate = false;
