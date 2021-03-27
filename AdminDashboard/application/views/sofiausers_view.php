50f69450fa2a5c31f823aea36fed7fbac9367d85
SergeyShishenko <serge-meb@mail.ru>
2021-03-27 21:46:05 +0300

    users

--- AdminDashboard/application/views/sofiausers_view.php
+++ AdminDashboard/application/views/sofiausers_view.php
@@ -11,10 +11,11 @@
         <table class="table table-bordered table-striped table-condensed mb-none">
             <thead>
                 <tr>
-                    <th class="hidden-xs hidden-sm">Company</th>
-                    <th class="text-right">Price</th>
-                    <th class="text-right hidden-xs hidden-sm">Change</th>
-                    <th class="text-right hidden-xs">Change %</th>
+                    <th class="hidden-xs hidden-sm">user_login</th>
+                    <th class="hidden-xs hidden-sm">user_mail</th>
+                    <th class="text-right">user_role</th>
+                    <th class="text-right hidden-xs hidden-sm">user_sofia_id</th>
+                    <th class="text-right hidden-xs">user_last_page</th>
                     <th class="text-right">Open</th>
                     <th class="text-right hidden-xs hidden-sm">High</th>
                     <th class="text-right hidden-xs hidden-sm">Low</th>
@@ -28,11 +29,11 @@
 	{
 		echo 
 		'<tr>
-			<td>'.$row['Code'].'</td>
-			<td class="hidden-xs hidden-sm">'.$row['Company'].'.</td>
-			<td class="text-right">'.$row['Price'].'</td>
-			<td class="text-right hidden-xs hidden-sm">'.$row['Change'].'</td>
-			<td class="text-right hidden-xs">'.$row['Change %'].'</td>
+			<td>'.$row['user_login'].'</td>
+			<td class="hidden-xs hidden-sm">'.$row['user_mail'].'.</td>
+			<td class="text-right">'.$row['user_role'].'</td>
+			<td class="text-right hidden-xs hidden-sm">'.$row['user_sofia_id'].'</td>
+			<td class="text-right hidden-xs">'.$row['user_last_page'].'</td>
 			<td class="text-right">'.$row['Open'].'</td>
 			<td class="text-right hidden-xs hidden-sm">'.$row['High'].'</td>
 			<td class="text-right hidden-xs hidden-sm">'.$row['Low'].'</td>
