f7e90f26b6991e9790ddc199444a232a55d7603a
Шишенко Сергей <sshishenko@sd.net>
2021-03-31 15:10:17 +0400

    svg

--- AdminDashboard/application/views/login_view.php
+++ AdminDashboard/application/views/login_view.php
@@ -7,23 +7,31 @@
             </tr>
             <tr>
                 <td>Логин</td>
-                <td><input type="text" name="login" autocomplete="new-password"></td>
+                <td><input type="text" name="login" autocomplete="new-password" value="<?php echo $_POST['login'] ?>">
+                </td>
             </tr>
             <tr>
                 <td>Пароль</td>
-                <td><input type="password" name="password" autocomplete="new-password"></td>
+                <td>
+                    <div class="password">
+                        <input type="password" id="password-input" name="password" autocomplete="new-password"
+                            value="<?php echo  ($data['generateCode'][0]) ? $data['generateCode'][0] : $_POST['password'] ?>">
+                        <a href="#" class="password-control" onclick="return show_hide_password(this);"></a>
+                    </div>
+                </td>
+
             </tr>
             <th colspan="2" style="text-align: right">
                 <input type="submit" value="Зарегистрировать" name="btn" style="width: 150px; height: 30px;"></th>
         </table>
-    </form>
+        <!-- </form> -->
 </p>
-<form action="" method="post">
+<!-- <form action="" method="post"> -->
 Сгенерированный пароль <input id="generatepass" name="generatepass" readonly type="text"
     value="<?php echo $data['generateCode'][0] ?>" disabled><br><br>
 
-<input name="generatesubmit" type="submit" value="Сгенерировать пароль" ><br><br>
-</form>	
+<input name="generatesubmit" type="submit" value="Сгенерировать пароль"><br><br>
+</form>
 <!-- <input name="generatesubmit" type="submit" value="Сгенерировать пароль" onClick="generateCode()"><br><br>
 <input name="changepass" type="submit" value="Изменить пароль" onClick="changepass()"><br><br>
 <input name="readlogin" type="submit" value="Данные Логина" onClick="readlogin()"><br><br> -->
@@ -35,21 +43,18 @@ if (window.history.replaceState) {
     window.history.replaceState(null, null, window.location.href);
 }
 
-// function generateCode() {
-//     var request = new XMLHttpRequest();
-//     request.open('GET', 'generateCode.php', true);
-//     request.addEventListener('readystatechange', function() {
-//         if ((request.readyState == 4) && (request.status == 200)) {
-//             var welcome = document.getElementById('generatepass');
-//             var welcome2 = document.getElementById('password');
-//             welcome.value = request.responseText;
-//             welcome.readOnly = true;
-//             welcome.disabled = true;
-//             welcome2.value = request.responseText;
-//         }
-//     });
-//     request.send();
-// }
+
+function show_hide_password(target) {
+    var input = document.getElementById('password-input');
+    if (input.getAttribute('type') == 'password') {
+        target.classList.add('view');
+        input.setAttribute('type', 'text');
+    } else {
+        target.classList.remove('view');
+        input.setAttribute('type', 'password');
+    }
+    return false;
+}
 </script>
 <?php
 
@@ -57,7 +62,7 @@ if (isset($data)){
 	// echo "<pre>";
 	// var_dump($data);
 	// echo "</pre>";
-	// echo $data['generateCode'][0]."<br>";
+	// echo isset($data['generateCode'][0])."<br>";
 	foreach($data as $key){
 		// echo'<span style="color:blue">'.key($data).'</span><br>';
 	if (key($data)!=="generateCode"){
