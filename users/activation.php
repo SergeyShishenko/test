ae2f6b842c243f2e4741ef059382dbc20bb5c5e6
Шишенко Сергей <sshishenko@sd.net>
2021-05-13 15:05:15 +0400

    users

--- users/activation.php
+++ users/activation.php
@@ -1,39 +1,5 @@
 <?php
 // users\activation.php активация почты
 session_start();
- require_once (dirname(dirname(dirname(__FILE__))).'/DATA/TABLES/configDB.php'); // подключение к базе данных
- $dbconn=dbconnect();
- $err = []; 
- $msg='';
- if(!empty($_GET['do']) && isset($_GET['do']))
- {
- $code=mysqli_real_escape_string($dbconn,$_GET['do']);
- $c=mysqli_query($dbconn,"SELECT `user_sofia_id` FROM `sofia_users` WHERE `user_activation`='$code'");
-
-    if(mysqli_num_rows($c) > 0)
-    {
-    $count=mysqli_query($dbconn,"SELECT `user_sofia_id` FROM `sofia_users` WHERE `user_activation`='$code' AND `user_status`= 0");
-
-        if(mysqli_num_rows($count) == 1)
-        {
-        mysqli_query($dbconn,"UPDATE sofia_users SET `user_status`= 1 WHERE user_activation='$code'");
-        header('Location:../messages/messages.php?msg=1'); 
-        // $msg="Ваш email подтвержден."; //msg=1
-        }
-        else
-        {
-            header('Location:../messages/messages.php?msg=2'); 
-        // $msg ="Ваш email уже подтвержден.";//msg=2
-        }
-    }else{
-        header('Location:../messages/messages.php?msg=3'); 
-        // $msg ="Неверный код подтверждения.";//msg=3
-        }
- 
- }else{
-    header('Location:../messages/messages.php?msg=3'); 
-    // $msg ="Неверный код подтверждения.";//msg=13
-    }
+include dirname(dirname(dirname(__FILE__))).'/DATA/TABLES/users/activation.php'
 ?>
-
-<?php //echo $msg; ?>
--- users/checkemailaccess.php
+++ users/checkemailaccess.php
@@ -1,27 +1,4 @@
 <?php 
 // users\checkemailaccess.php
-require_once (dirname(dirname(dirname(__FILE__))).'/DATA/TABLES/configDB.php'); // подключение к базе данных
-$dbconn=dbconnect();
-if(isset($_POST['login']) && isset($_POST['Email'])){
-    $mailadres=mysqli_real_escape_string($dbconn,$_POST['Email']);
-    $login =mysqli_real_escape_string($dbconn,$_POST['login']);;
-    $res=mysqli_query($dbconn,"SELECT * FROM `sofia_users`  WHERE `user_login` = '$login'");
-    if(mysqli_num_rows($res) > 0)
-    {
-        $data = mysqli_fetch_assoc($res);
-        if ($data['user_status']==1 ){
-         echo 1;
-        }else{
-            echo 0; 
-        }
-
-    }else{
-        printf("Ошибка: %s\n", mysqli_error($dbconn));
-    exit();
-    } 
-    
-}else{
-    echo 2; 
-}
-
+include dirname(dirname(dirname(__FILE__))).'/DATA/TABLES/users/checkemailaccess.php'
 ?>
