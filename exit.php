066207a7bd17a1e60c5355c74b05867462624c58
Шишенко Сергей <sshishenko@sd.net>
2021-05-18 16:47:51 +0400

    INDEX

--- exit.php
+++ exit.php
@@ -1,28 +1,10 @@
 <?php
-session_start();
-// echo'<pre>';
-// Print_r ($_SESSION);
-// echo'</pre>';
-// очистить таблицу user_vpi
-// define('__ROOT__', dirname(dirname(dirname(__FILE__)))); 
-require_once(dirname(dirname(__FILE__)).'/DATA/TABLES/configDB.php'); // подключение к базе данных
-$dbconn=dbconnect();
-$hash=mysqli_real_escape_string($dbconn, $_COOKIE['hash']);
-// $hash=$_COOKIE["hash"];
-mysqli_query($dbconn,"DELETE FROM `user_vpi` WHERE `vpi_hash_id`='$hash'");
-mysqli_close($dbconn); 
-unset($GLOBALS['dbconn']);
-// Удаляем cookies
-if (isset($_SERVER['HTTP_COOKIE'])) {
-    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
-    foreach($cookies as $cookie) {
-        $parts = explode('=', $cookie);
-        $name = trim($parts[0]);
-        setcookie($name, '', time()-1000);
-        setcookie($name, '', time()-1000, '/');
+// exit.php
+if(file_exists($_SERVER['DOCUMENT_ROOT'] . '/www/index.php')){ 
+    //  echo  "local"; exit();
+        include  dirname(dirname(__FILE__)).'/DATA/TABLES/exit.php';//local
+    }else{
+      include   dirname(dirname(dirname(__FILE__))).'/DATA/TABLES/exit.php';
     }
-}
-session_unset(); // Удаляем все переменные
-session_destroy();
 header("Location: index-session.php");
 ?>
--- inc/inc-session-start.php
+++ inc/inc-session-start.php
@@ -1,42 +1,12 @@
 <?php
-ini_set('session.gc_maxlifetime', 43200);// 12 часов
-// ini_set('session.cookie_lifetime', 0);
-// session_set_cookie_params(0);
-session_start();
-// $s_id=session_id();
-
-$ref = $_SERVER['REQUEST_URI'];
-
-$_SESSION['ref'] = $ref;
-
-define('__ROOT__', dirname(dirname(dirname(__FILE__)))); // было
-
-require_once(__ROOT__.'/DATA/TABLES/configDB.php'); // подключение к базе данных 
-
-$dbconn=dbconnect();
-
-$hash=mysqli_real_escape_string($dbconn, $_COOKIE['hash']);
-
-if (is_null($hash)){ $hash="-"; }
-
-
-$result = mysqli_query($dbconn,"SELECT * FROM `sofia_users` WHERE `user_hash` = '$hash'");
-   
-    if (mysqli_num_rows($result) == 0) {//есть запись     
-
-        if(!isset($_SESSION['sess_login']) || !isset($_SESSION['sess_pass']) ) {
-            if (basename($ref)=="CNC"){
-                header('Location:../index-session.php');    exit();
-            }
-             header('Location:index-session.php');    exit();
-            }     
-
+// inc\inc-session-start.php
+if(file_exists($_SERVER['DOCUMENT_ROOT'] . '/www/index.php')){ 
+    //  echo  "local"; exit();
+        $role = include  dirname(dirname(dirname(__FILE__))).'/DATA/TABLES/inc-session-start.php';//local
+    }else{
+        $role = include  dirname( dirname(dirname(dirname(__FILE__)))).'/DATA/TABLES/inc-session-start.php';
     }
-    $data = mysqli_fetch_assoc($result);
-    $role=$data['user_role']; 
-
-    
-
-    // mysqli_close($dbconn);  
+    return $role;
+ 
   
 
