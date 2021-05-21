bce61521a74702decc7c134e810b8c8552825dbf
Шишенко Сергей <sshishenko@sd.net>
2021-05-21 15:03:40 +0400

    db

--- inc/inc-menu-value.php
+++ inc/inc-menu-value.php
@@ -1,4 +1,5 @@
 <?php
+// inc\inc-menu-value.php
 $detaliClass = '';
 $assemblyUnitClass = '';
 $izdeliyaClass = '';
@@ -9,9 +10,10 @@ $metodichkiClass = '';
 $furnituraClass = '';
 $materialyClass = '';
 $literaturaClass = '';
-if (isset($escape_string_login)){
-$result = mysqli_query($dbconn,"UPDATE `sofia_users` SET `user_sess_date_start` = CURRENT_TIMESTAMP, `user_last_page` = '$ref' WHERE user_login='$escape_string_login'");
-// setcookie("ref", $_SERVER['REQUEST_URI'], time()+60*60*24*30);// 24 часа
-mysqli_close($dbconn);  
-unset($GLOBALS['dbconn']);
-}
+if(file_exists($_SERVER['DOCUMENT_ROOT'] . '/www/index.php')){ 
+    //  echo  "local"; exit();
+        include  dirname(dirname(dirname(__FILE__))).'/DATA/TABLES/inc-menu-value.php';//local
+    }else{
+        include  dirname( dirname(dirname(dirname(__FILE__)))).'/DATA/TABLES/inc-menu-value.php';
+    }
+   
