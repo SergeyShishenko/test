eca438869f6e35644446fa5d12f184e90918b31b
Шишенко Сергей <sshishenko@sd.net>
2021-06-08 15:01:19 +0400

    tables

--- exit.php
+++ exit.php
@@ -5,25 +5,27 @@ session_start();
 // echo'</pre>';
 // очистить таблицу user_vpi
 // define('__ROOT__', dirname(dirname(dirname(__FILE__)))); 
-require_once(dirname(dirname(__FILE__)).'/DATA/TABLES/configDB.php'); // подключение к базе данных
-$dbconn=dbconnect();
-$hash=$_COOKIE["hash"];
-mysqli_query($dbconn,"DELETE FROM `user_vpi` WHERE `vpi_hash_id`='$hash'");
-mysqli_close($dbconn); 
-// Удаляем cookies
-if (isset($_SERVER['HTTP_COOKIE'])) {
-    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
-    foreach($cookies as $cookie) {
-        $parts = explode('=', $cookie);
-        $name = trim($parts[0]);
-        setcookie($name, '', time()-1000);
-        setcookie($name, '', time()-1000, '/');
-    }
-}
-session_unset(); // Удаляем все переменные
-session_destroy();
-header("Location: index-session.php");
 
+/////////////
+// require_once(dirname(dirname(__FILE__)).'/DATA/TABLES/configDB.php'); // подключение к базе данных
+// $dbconn=dbconnect();
+// $hash=$_COOKIE["hash"];
+// mysqli_query($dbconn,"DELETE FROM `user_vpi` WHERE `vpi_hash_id`='$hash'");
+// mysqli_close($dbconn); 
+// // Удаляем cookies
+// if (isset($_SERVER['HTTP_COOKIE'])) {
+//     $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+//     foreach($cookies as $cookie) {
+//         $parts = explode('=', $cookie);
+//         $name = trim($parts[0]);
+//         setcookie($name, '', time()-1000);
+//         setcookie($name, '', time()-1000, '/');
+//     }
+// }
+// session_unset(); // Удаляем все переменные
+// session_destroy();
+// header("Location: index-session.php");
+///////////////
 
 
 
@@ -31,11 +33,11 @@ header("Location: index-session.php");
 
 
 // exit.php
-// if(file_exists($_SERVER['DOCUMENT_ROOT'] . '/www/index.php')){ 
-//     //  echo  "local"; exit();
-//         include  dirname(dirname(__FILE__)).'/DATA/TABLES/exit.php';//local
-//     }else{
-//       include   dirname(dirname(dirname(__FILE__))).'/DATA/TABLES/exit.php';
-//     }
-// header("Location: index-session.php");
+if(file_exists($_SERVER['DOCUMENT_ROOT'] . '/www/index.php')){ 
+    //  echo  "local"; exit();
+        include  dirname(dirname(__FILE__)).'/DATA/TABLES/exit.php';//local
+    }else{
+      include   dirname(dirname(dirname(__FILE__))).'/DATA/TABLES/exit.php';
+    }
+header("Location: index-session.php");
 ?>
