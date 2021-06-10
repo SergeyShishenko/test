f53d6b07897ce1647d0e0994b7198a6274cf44a0
SergeyShishenko <serge-meb@mail.ru>
2021-06-10 13:23:55 +0400

    icq

--- ICQ/ICQ-BOT.php
+++ ICQ/ICQ-BOT.php
@@ -1,6 +1,9 @@
 <?php
 // F:\OpenServer_5.3.8\domains\localhost\www\ICQ\ICQ-BOT.php
 // ICQ-Bot, посылаем сообщение пользователю или в групповой чат
+
+// https://icq.com/botapi/#/messages/get_messages_sendFile
+
 $msg = "Привет! Я от Сергея Шишенко!";
 $ICQ_BOTTOKEN = "001.3889243197.2133753185:759985177"; //Bot token
 $ICQ_IDCHAT = "@sh79037727187"; //ChatID, ник пользователя
