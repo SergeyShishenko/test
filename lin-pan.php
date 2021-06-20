818114e92189b303909d08602cb438eb64ba7ef0
SergeyShishenko <serge-meb@mail.ru>
2021-06-20 14:22:48 +0300

    lin

--- lin-pan.php
+++ lin-pan.php
@@ -1,4 +1,15 @@
 <?php
 // log in to the panel
-header('Location:AdminDashboard/');    exit();
+include 'inc/inc-session-start.php';
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! проверка на session['admin']
+if(file_exists($_SERVER['DOCUMENT_ROOT'] . '/www/index.php')){ 
+    //  echo  "local"; exit();
+    $site=dirname(dirname(__FILE__)).'/DATA/TABLES/icq/';//local       
+    }else{
+     $site=dirname(dirname(dirname(__FILE__))).'/DATA/TABLES/icq/';          
+    }
+include  $site.'/ajax-s.php';
+
+// header('Location:AdminDashboard/');    exit();
+exit();
 ?>
