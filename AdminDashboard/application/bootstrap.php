74828bd13339177418edb40c7932abc5398eb148
SergeyShishenko <serge-meb@mail.ru>
2021-08-06 10:22:10 +0400

    route

--- AdminDashboard/application/bootstrap.php
+++ AdminDashboard/application/bootstrap.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 // подключаем файлы ядра
 // define('PATH__CORE', dirname(dirname(dirname(dirname(__FILE__)))) . '/DATA/TABLES/'); 
 define('PATH__CORE',  $_SERVER['DOCUMENT_ROOT'] . '/DATA/TABLES/'); 
@@ -8,6 +9,19 @@ include PATH__CORE .'class_QuerySofia.php';
 require_once PATH__CORE .'core/model.php';
 require_once PATH__CORE .'core/view.php';
 require_once PATH__CORE .'core/controller.php';
+define('BASE__NAME',basename(dirname(__DIR__, 1)));
+// $_SESSION['BASE__NAME'] = BASE__NAME;
+/**
+ * Изменить в файле .htaccess !!!!!!!!!!!!!!!!!!!!! AdminDashboard
+
+ * F:\OpenServer_5.3.8\domains\localhost\www\AdminDashboard\.htaccess
+ * ErrorDocument 403 http://localhost/www/AdminDashboard/err/error403.php 
+
+ */
+
+
+// echo $_SESSION['BASE__NAME'];
+// exit();
 /*
 Здесь обычно подключаются дополнительные модули, реализующие различный функционал:
 	> аутентификацию
@@ -38,3 +52,4 @@ require_once PATH__CORE .'core/route.php';
 define('__ROOTR__', dirname(dirname(__FILE__)));
 // echo  __ROOTR__."<br>";
 Route::start(); // запускаем маршрутизатор
+// Route::start(); // запускаем маршрутизатор
