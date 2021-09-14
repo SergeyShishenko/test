54397dcf6281ad8023825688ca07c214356b3ceb
SergeyShishenko <serge-meb@mail.ru>
2021-09-14 07:57:18 +0400

    tg

--- tgaccess.php
+++ tgaccess.php
@@ -1,3 +1,8 @@
 <?php
-echo "ok!";
+if(file_exists($_SERVER['DOCUMENT_ROOT'] . '/www/index.php')){ 
+    //  echo  "local"; exit();
+        include  dirname(dirname(__FILE__)).'/DATA/TABLES/tgaccess.php';//local
+    }else{
+      include   dirname(dirname(dirname(__FILE__))).'/DATA/TABLES/tgaccess.php';
+    }
 ?>
